89cc8b0687a1543d83bd9dff444d25f864a37251
Oleksii Dykan <alick_bass@mail.ru>
2017-05-01 15:53:44 +0200

    Make Component Equatable

--- ViewComponents/Component.swift
+++ ViewComponents/Component.swift
@@ -11,9 +11,10 @@ import UIKit
 protocol ComponentType {
     var styles: [StyleType] { get }
     func configure(view: UIView)
+    func isEqual(to other: ComponentType) -> Bool
 }
 
-struct ChildComponent {
+struct ChildComponent: ComponentType {
     let component: ComponentType
     let access: (UIView) -> UIView
     
@@ -21,17 +22,29 @@ struct ChildComponent {
         self.component = component
         self.access = { view in access(view as! T) }
     }
+    
+    var styles: [StyleType] {
+        return component.styles
+    }
+    
+    func configure(view: UIView) {
+        component.configure(view: access(view))
+    }
+    
+    func isEqual(to other: ComponentType) -> Bool {
+        return component.isEqual(to: other)
+    }
 }
 
 public struct Component<T: UIView>: ComponentType {
     public let styles: [StyleType]
-    let children: [ChildComponent]
+    let children: [ComponentType]
     
     public init() {
         self.init(styles: [], children: [])
     }
     
-    init(styles: [StyleType], children: [ChildComponent]) {
+    init(styles: [StyleType], children: [ComponentType]) {
         self.styles = styles
         self.children = children
     }
@@ -42,10 +55,24 @@ public struct Component<T: UIView>: ComponentType {
     
     public func configure(view: T) {
         styles.forEach { $0.sideEffect(view: view) }
-        children.forEach({ $0.component.configure(view: $0.access(view)) })
+        children.forEach({ $0.configure(view: view) })
     }
     
     public func child<V: UIView>(_ component: Component<V>, _ access: @escaping (T) -> V) -> Component<T> {
         return Component<T>(styles: styles, children: children + [ChildComponent(component: component, access)])
+    }
+}
+
+extension Component: Equatable {
+    func isEqual(to other: ComponentType) -> Bool {
+        guard let other = other as? Component<T> else { return false }
+        return self == other
+    }
+    
+    public static func == (lhs: Component<T>, rhs: Component<T>) -> Bool {
+        return lhs.styles.count == rhs.styles.count
+            && lhs.children.count == rhs.children.count
+            && zip(lhs.styles, rhs.styles).reduce(true, { $0 && $1.0.isEqual(to: $1.1) })
+            && zip(lhs.children, rhs.children).reduce(true, { $0 && $1.0.isEqual(to: $1.1) })
     }
 }
