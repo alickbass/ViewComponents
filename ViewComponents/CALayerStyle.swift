db3c0f545e02215f492525dd66fc19253ae092d5
Oleksii Dykan <alick_bass@mail.ru>
2017-05-10 16:10:19 +0200

    Cover CALayerStyle side effects with unit tests

--- ViewComponents/CALayerStyle.swift
+++ ViewComponents/CALayerStyle.swift
@@ -192,3 +192,9 @@ public enum CALayerStyle: ConcreteStyleType {
         }
     }
 }
+
+public extension Component {
+    public func layerStyles(_ styles: CALayerStyle...) -> Component<T> {
+        return Component<T>(styles: self.styles + styles, children: children)
+    }
+}
--- ViewComponentsTests/TestCALayerStyle.swift
+++ ViewComponentsTests/TestCALayerStyle.swift
@@ -78,4 +78,109 @@ class TestCALayerStyle: XCTestCase {
         XCTAssertEqual(CALayerStyle.name("test"), .name("test"))
         XCTAssertNotEqual(CALayerStyle.name("test"), .contentsScale(0.2))
     }
+    
+    func testStyleSideEffects() {
+        let view = UIView()
+        
+        view.layer.contentsGravity = kCAGravityCenter
+        CALayerStyle.contentsGravity(.bottom).sideEffect(on: view)
+        XCTAssertEqual(view.layer.contentsGravity, kCAGravityBottom)
+        
+        view.layer.opacity = 1
+        CALayerStyle.opacity(0.2).sideEffect(on: view)
+        XCTAssertEqual(view.layer.opacity, 0.2)
+        
+        view.layer.isHidden = false
+        CALayerStyle.isHidden(true).sideEffect(on: view)
+        XCTAssertEqual(view.layer.isHidden, true)
+        
+        view.layer.masksToBounds = false
+        CALayerStyle.masksToBounds(true).sideEffect(on: view)
+        XCTAssertEqual(view.layer.masksToBounds, true)
+        
+        let mask = CALayer()
+        view.layer.mask = mask
+        CALayerStyle.mask(mask).sideEffect(on: view)
+        XCTAssertEqual(view.layer.mask, mask)
+        
+        view.layer.isDoubleSided = false
+        CALayerStyle.isDoubleSided(true).sideEffect(on: view)
+        XCTAssertEqual(view.layer.isDoubleSided, true)
+        
+        view.layer.backgroundColor = UIColor.red.cgColor
+        CALayerStyle.backgroundColor(.green).sideEffect(on: view)
+        XCTAssertEqual(view.layer.backgroundColor, UIColor.green.cgColor)
+        
+        view.layer.allowsEdgeAntialiasing = false
+        CALayerStyle.allowsEdgeAntialiasing(true).sideEffect(on: view)
+        XCTAssertEqual(view.layer.allowsEdgeAntialiasing, true)
+        
+        view.layer.allowsGroupOpacity = false
+        CALayerStyle.allowsGroupOpacity(true).sideEffect(on: view)
+        XCTAssertEqual(view.layer.allowsGroupOpacity, true)
+        
+        view.layer.isOpaque = false
+        CALayerStyle.isOpaque(true).sideEffect(on: view)
+        XCTAssertEqual(view.layer.isOpaque, true)
+        
+        view.layer.edgeAntialiasingMask = .layerTopEdge
+        CALayerStyle.edgeAntialiasingMask(.layerLeftEdge).sideEffect(on: view)
+        XCTAssertEqual(view.layer.edgeAntialiasingMask, .layerLeftEdge)
+        
+        view.layer.isGeometryFlipped = false
+        CALayerStyle.isGeometryFlipped(true).sideEffect(on: view)
+        XCTAssertEqual(view.layer.isGeometryFlipped, true)
+        
+        view.layer.drawsAsynchronously = false
+        CALayerStyle.drawsAsynchronously(true).sideEffect(on: view)
+        XCTAssertEqual(view.layer.drawsAsynchronously, true)
+        
+        view.layer.shouldRasterize = false
+        CALayerStyle.shouldRasterize(true).sideEffect(on: view)
+        XCTAssertEqual(view.layer.shouldRasterize, true)
+        
+        view.layer.rasterizationScale = 1
+        CALayerStyle.rasterizationScale(0.2).sideEffect(on: view)
+        XCTAssertEqual(view.layer.rasterizationScale, 0.2)
+        
+        view.layer.contentsFormat = kCAContentsFormatRGBA16Float
+        CALayerStyle.contentsFormat(.RGBA8Uint).sideEffect(on: view)
+        XCTAssertEqual(view.layer.contentsFormat, kCAContentsFormatRGBA8Uint)
+        
+        view.layer.frame = CGRect(x: 20, y: 20, width: 20, height: 20)
+        CALayerStyle.frame(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.layer.frame, .zero)
+        
+        view.layer.bounds = CGRect(x: 20, y: 20, width: 20, height: 20)
+        CALayerStyle.bounds(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.layer.bounds, .zero)
+        
+        view.layer.position = CGPoint(x: 20, y: 20)
+        CALayerStyle.position(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.layer.position, .zero)
+        
+        view.layer.zPosition = 1
+        CALayerStyle.zPosition(0.2).sideEffect(on: view)
+        XCTAssertEqual(view.layer.zPosition, 0.2)
+        
+        view.layer.anchorPointZ = 1
+        CALayerStyle.anchorPointZ(0.2).sideEffect(on: view)
+        XCTAssertEqual(view.layer.anchorPointZ, 0.2)
+        
+        view.layer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        CALayerStyle.anchorPoint(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.layer.anchorPoint, .zero)
+        
+        view.layer.contentsScale = 1
+        CALayerStyle.contentsScale(0.2).sideEffect(on: view)
+        XCTAssertEqual(view.layer.contentsScale, 0.2)
+        
+        view.layer.name = nil
+        CALayerStyle.name("test").sideEffect(on: view)
+        XCTAssertEqual(view.layer.name, "test")
+        
+        view.layer.name = nil
+        Component<UIView>().layerStyles(.name("test")).configure(view: view)
+        XCTAssertEqual(view.layer.name, "test")
+    }
 }
