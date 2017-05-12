395127aa107a7e5b8f5aaa50ee395f44c984cc02
Oleksii Dykan <alick_bass@mail.ru>
2017-05-12 14:24:53 +0200

    Add table view component styling

--- ViewComponents/TableViewStyle.swift
+++ ViewComponents/TableViewStyle.swift
@@ -155,3 +155,9 @@ public enum TableViewStyle: HashableConcreteStyle {
         }
     }
 }
+
+public extension Component where T: UITableView {
+    public func tableViewStyles(_ styles: TableViewStyle...) -> Component<T> {
+        return add(styles: styles)
+    }
+}
