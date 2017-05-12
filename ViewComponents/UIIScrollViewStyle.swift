41dc938f8e507d67d1f79a68ce433b66c027175c
Oleksii Dykan <alick_bass@mail.ru>
2017-05-12 12:00:40 +0200

    Add UIScrollViewStyle

---
+++ ViewComponents/UIIScrollViewStyle.swift
@@ -0,0 +1,151 @@
+//
+//  UIIScrollViewStyle.swift
+//  ViewComponents
+//
+//  Created by Oleksii on 12/05/2017.
+//  Copyright © 2017 WeAreReasonablePeople. All rights reserved.
+//
+
+import UIKit
+
+public enum UIScrollViewStyle: HashableConcreteStyle {
+    case contentOffset(CGPoint)
+    case contentSize(CGSize)
+    case contentInset(UIEdgeInsets)
+    case isScrollEnabled(Bool)
+    case isDirectionalLockEnabled(Bool)
+    case isPagingEnabled(Bool)
+    case bounces(Bool)
+    case alwaysBounceVertical(Bool)
+    case alwaysBounceHorizontal(Bool)
+    case canCancelContentTouches(Bool)
+    case delaysContentTouches(Bool)
+    case indicatorStyle(UIScrollViewIndicatorStyle)
+    case scrollIndicatorInsets(UIEdgeInsets)
+    case showsHorizontalScrollIndicator(Bool)
+    case showsVerticalScrollIndicator(Bool)
+    case maximumZoomScale(CGFloat)
+    case minimumZoomScale(CGFloat)
+    case bouncesZoom(Bool)
+
+    public func sideEffect(on view: UIScrollView) {
+        switch self {
+        case let .contentOffset(value):
+            view.contentOffset = value
+        case let .contentSize(value):
+            view.contentSize = value
+        case let .contentInset(value):
+            view.contentInset = value
+        case let .isScrollEnabled(value):
+            view.isScrollEnabled = value
+        case let .isDirectionalLockEnabled(value):
+            view.isDirectionalLockEnabled = value
+        case let .isPagingEnabled(value):
+            view.isPagingEnabled = value
+        case let .bounces(value):
+            view.bounces = value
+        case let .alwaysBounceVertical(value):
+            view.alwaysBounceVertical = value
+        case let .alwaysBounceHorizontal(value):
+            view.alwaysBounceHorizontal = value
+        case let .canCancelContentTouches(value):
+            view.canCancelContentTouches = value
+        case let .delaysContentTouches(value):
+            view.delaysContentTouches = value
+        case let .indicatorStyle(value):
+            view.indicatorStyle = value
+        case let .scrollIndicatorInsets(value):
+            view.scrollIndicatorInsets = value
+        case let .showsHorizontalScrollIndicator(value):
+            view.showsHorizontalScrollIndicator = value
+        case let .showsVerticalScrollIndicator(value):
+            view.showsVerticalScrollIndicator = value
+        case let .maximumZoomScale(value):
+            view.maximumZoomScale = value
+        case let .minimumZoomScale(value):
+            view.minimumZoomScale = value
+        case let .bouncesZoom(value):
+            view.bouncesZoom = value
+        }
+    }
+    
+    public static func == (lhs: UIScrollViewStyle, rhs: UIScrollViewStyle) -> Bool {
+        switch (lhs, rhs) {
+        case let (.contentOffset(left), .contentOffset(right)):
+            return left == right
+        case let (.contentSize(left), .contentSize(right)):
+            return left == right
+        case let (.contentInset(left), .contentInset(right)),
+             let (.scrollIndicatorInsets(left), .scrollIndicatorInsets(right)):
+            return left == right
+        case let (.isScrollEnabled(left), .isScrollEnabled(right)),
+             let (.isDirectionalLockEnabled(left), .isDirectionalLockEnabled(right)),
+             let (.isPagingEnabled(left), .isPagingEnabled(right)),
+             let (.bounces(left), .bounces(right)),
+             let (.alwaysBounceVertical(left), .alwaysBounceVertical(right)),
+             let (.alwaysBounceHorizontal(left), .alwaysBounceHorizontal(right)),
+             let (.canCancelContentTouches(left), .canCancelContentTouches(right)),
+             let (.delaysContentTouches(left), .delaysContentTouches(right)),
+             let (.showsHorizontalScrollIndicator(left), .showsHorizontalScrollIndicator(right)),
+             let (.showsVerticalScrollIndicator(left), .showsVerticalScrollIndicator(right)),
+             let (.bouncesZoom(left), .bouncesZoom(right)):
+            return left == right
+        case let (.maximumZoomScale(left), .maximumZoomScale(right)),
+             let (.minimumZoomScale(left), .minimumZoomScale(right)):
+            return left == right
+        case let (.indicatorStyle(left), .indicatorStyle(right)):
+            return left == right
+        default:
+            return false
+        }
+    }
+    
+    var startIndex: Int { return 69 }
+    
+    var value: (index: Int, valueHash: Int) {
+        switch self {
+        case let .contentOffset(value):
+            return (1, value.hashValue)
+        case let .contentSize(value):
+            return (2, value.hashValue)
+        case let .contentInset(value):
+            return (3, value.hashValue)
+        case let .isScrollEnabled(value):
+            return (4, value.hashValue)
+        case let .isDirectionalLockEnabled(value):
+            return (5, value.hashValue)
+        case let .isPagingEnabled(value):
+            return (6, value.hashValue)
+        case let .bounces(value):
+            return (7, value.hashValue)
+        case let .alwaysBounceVertical(value):
+            return (8, value.hashValue)
+        case let .alwaysBounceHorizontal(value):
+            return (9, value.hashValue)
+        case let .canCancelContentTouches(value):
+            return (10, value.hashValue)
+        case let .delaysContentTouches(value):
+            return (11, value.hashValue)
+        case let .indicatorStyle(value):
+            return (12, value.hashValue)
+        case let .scrollIndicatorInsets(value):
+            return (13, value.hashValue)
+        case let .showsHorizontalScrollIndicator(value):
+            return (14, value.hashValue)
+        case let .showsVerticalScrollIndicator(value):
+            return (15, value.hashValue)
+        case let .maximumZoomScale(value):
+            return (16, value.hashValue)
+        case let .minimumZoomScale(value):
+            return (17, value.hashValue)
+        case let .bouncesZoom(value):
+            return (28, value.hashValue)
+        }
+    }
+}
+
+public extension Component where T: UIScrollView {
+    public func scrollViewStyles(_ styles: UIScrollViewStyle...) -> Component<T> {
+        return add(styles: styles)
+    }
+}
---
+++ ViewComponentsTests/TestUIScrollViewStyle.swift
@@ -0,0 +1,137 @@
+//
+//  TestUIScrollViewStyle.swift
+//  ViewComponents
+//
+//  Created by Oleksii on 12/05/2017.
+//  Copyright © 2017 WeAreReasonablePeople. All rights reserved.
+//
+
+import XCTest
+import ViewComponents
+
+class TestUIScrollViewStyle: XCTestCase {
+    
+    static let allStyles: [UIScrollViewStyle] = [
+        .contentOffset(.zero), .contentSize(.zero), .contentInset(.zero), .isScrollEnabled(true),
+        .isDirectionalLockEnabled(true), .isPagingEnabled(true), .bounces(true), .alwaysBounceVertical(true),
+        .alwaysBounceHorizontal(true), .canCancelContentTouches(true), .delaysContentTouches(true),
+        .indicatorStyle(.black), .scrollIndicatorInsets(.zero), .showsHorizontalScrollIndicator(true),
+        .showsVerticalScrollIndicator(true), .maximumZoomScale(0.4), .minimumZoomScale(0.4), .bouncesZoom(true)
+    ]
+    
+    static var accumulatedHashes: [Int] {
+        return TestUIImageViewStyle.accumulatedHashes + TestUIScrollViewStyle.allStyles.map({ $0.hashValue })
+    }
+    
+    func testStyleEquatable() {
+        XCTAssertEqual(UIScrollViewStyle.contentOffset(.zero), .contentOffset(.zero))
+        XCTAssertEqual(UIScrollViewStyle.contentSize(.zero), .contentSize(.zero))
+        XCTAssertEqual(UIScrollViewStyle.contentInset(.zero), .contentInset(.zero))
+        XCTAssertEqual(UIScrollViewStyle.isScrollEnabled(true), .isScrollEnabled(true))
+        XCTAssertEqual(UIScrollViewStyle.isDirectionalLockEnabled(true), .isDirectionalLockEnabled(true))
+        XCTAssertEqual(UIScrollViewStyle.isPagingEnabled(true), .isPagingEnabled(true))
+        XCTAssertEqual(UIScrollViewStyle.bounces(true), .bounces(true))
+        XCTAssertEqual(UIScrollViewStyle.alwaysBounceVertical(true), .alwaysBounceVertical(true))
+        XCTAssertEqual(UIScrollViewStyle.alwaysBounceHorizontal(true), .alwaysBounceHorizontal(true))
+        XCTAssertEqual(UIScrollViewStyle.canCancelContentTouches(true), .canCancelContentTouches(true))
+        XCTAssertEqual(UIScrollViewStyle.delaysContentTouches(true), .delaysContentTouches(true))
+        XCTAssertEqual(UIScrollViewStyle.indicatorStyle(.black), .indicatorStyle(.black))
+        XCTAssertEqual(UIScrollViewStyle.scrollIndicatorInsets(.zero), .scrollIndicatorInsets(.zero))
+        XCTAssertEqual(UIScrollViewStyle.showsHorizontalScrollIndicator(true), .showsHorizontalScrollIndicator(true))
+        XCTAssertEqual(UIScrollViewStyle.showsVerticalScrollIndicator(true), .showsVerticalScrollIndicator(true))
+        XCTAssertEqual(UIScrollViewStyle.maximumZoomScale(0.4), .maximumZoomScale(0.4))
+        XCTAssertEqual(UIScrollViewStyle.minimumZoomScale(0.4), .minimumZoomScale(0.4))
+        XCTAssertEqual(UIScrollViewStyle.bouncesZoom(true), .bouncesZoom(true))
+        XCTAssertNotEqual(UIScrollViewStyle.bouncesZoom(true), .minimumZoomScale(0.4))
+    }
+    
+    func testStyleSideEffects() {
+        let view = UIScrollView()
+        
+        view.contentOffset = CGPoint(x: 20, y: 20)
+        UIScrollViewStyle.contentOffset(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.contentOffset, .zero)
+        
+        view.contentSize = CGSize(width: 20, height: 20)
+        UIScrollViewStyle.contentSize(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.contentSize, .zero)
+        
+        view.contentInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        UIScrollViewStyle.contentInset(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.contentInset, .zero)
+        
+        view.isScrollEnabled = false
+        UIScrollViewStyle.isScrollEnabled(true).sideEffect(on: view)
+        XCTAssertEqual(view.isScrollEnabled, true)
+        
+        view.isDirectionalLockEnabled = false
+        UIScrollViewStyle.isDirectionalLockEnabled(true).sideEffect(on: view)
+        XCTAssertEqual(view.isDirectionalLockEnabled, true)
+        
+        view.isPagingEnabled = false
+        UIScrollViewStyle.isPagingEnabled(true).sideEffect(on: view)
+        XCTAssertEqual(view.isPagingEnabled, true)
+        
+        view.bounces = false
+        UIScrollViewStyle.bounces(true).sideEffect(on: view)
+        XCTAssertEqual(view.bounces, true)
+        
+        view.alwaysBounceVertical = false
+        UIScrollViewStyle.alwaysBounceVertical(true).sideEffect(on: view)
+        XCTAssertEqual(view.alwaysBounceVertical, true)
+        
+        view.alwaysBounceHorizontal = false
+        UIScrollViewStyle.alwaysBounceHorizontal(true).sideEffect(on: view)
+        XCTAssertEqual(view.alwaysBounceHorizontal, true)
+        
+        view.canCancelContentTouches = false
+        UIScrollViewStyle.canCancelContentTouches(true).sideEffect(on: view)
+        XCTAssertEqual(view.canCancelContentTouches, true)
+        
+        view.delaysContentTouches = false
+        UIScrollViewStyle.delaysContentTouches(true).sideEffect(on: view)
+        XCTAssertEqual(view.delaysContentTouches, true)
+        
+        view.indicatorStyle = .black
+        UIScrollViewStyle.indicatorStyle(.white).sideEffect(on: view)
+        XCTAssertEqual(view.indicatorStyle, .white)
+        
+        view.scrollIndicatorInsets = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        UIScrollViewStyle.scrollIndicatorInsets(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.scrollIndicatorInsets, .zero)
+        
+        view.showsHorizontalScrollIndicator = false
+        UIScrollViewStyle.showsHorizontalScrollIndicator(true).sideEffect(on: view)
+        XCTAssertEqual(view.showsHorizontalScrollIndicator, true)
+        
+        view.showsVerticalScrollIndicator = false
+        UIScrollViewStyle.showsVerticalScrollIndicator(true).sideEffect(on: view)
+        XCTAssertEqual(view.showsVerticalScrollIndicator, true)
+        
+        view.maximumZoomScale = 0.2
+        UIScrollViewStyle.maximumZoomScale(0.4).sideEffect(on: view)
+        XCTAssertEqual(view.maximumZoomScale, 0.4)
+        
+        view.minimumZoomScale = 0.2
+        UIScrollViewStyle.minimumZoomScale(0.4).sideEffect(on: view)
+        XCTAssertEqual(view.minimumZoomScale, 0.4)
+        
+        view.bouncesZoom = false
+        UIScrollViewStyle.bouncesZoom(true).sideEffect(on: view)
+        XCTAssertEqual(view.bouncesZoom, true)
+        
+        view.bouncesZoom = false
+        Component<UIScrollView>().scrollViewStyles(.bouncesZoom(true)).configure(view: view)
+        XCTAssertEqual(view.bouncesZoom, true)
+    }
+    
+    func testHashValue() {
+        var hashes = Set(TestUIImageViewStyle.accumulatedHashes)
+        
+        for item in TestUIScrollViewStyle.allStyles {
+            let hash = item.hashValue
+            XCTAssertFalse(hashes.contains(hash))
+            hashes.insert(hash)
+        }
+    }
+}
