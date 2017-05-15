63d1aa83e6337e39952291c72a5545323401e573
Oleksii Dykan <alick_bass@mail.ru>
2017-05-15 12:12:26 +0200

    Add Activity indicator styles

---
+++ ViewComponents/ActivityIndicatorViewStyle.swift
@@ -0,0 +1,75 @@
+//
+//  ActivityIndicatorViewStyle.swift
+//  ViewComponents
+//
+//  Created by Oleksii on 15/05/2017.
+//  Copyright © 2017 WeAreReasonablePeople. All rights reserved.
+//
+
+import UIKit
+
+public enum ActivityIndicatorViewStyle: HashableConcreteStyle {
+    case isAnimating(Bool)
+    case hidesWhenStopped(Bool)
+    case activityIndicatorViewStyle(UIActivityIndicatorViewStyle)
+    case color(UIColor?)
+    
+    public func sideEffect(on view: UIActivityIndicatorView) {
+        switch self {
+        case let .isAnimating(value):
+            switch value {
+            case true:
+                view.startAnimating()
+            case false:
+                view.stopAnimating()
+            }
+        case let .hidesWhenStopped(value):
+            view.hidesWhenStopped = value
+        case let .activityIndicatorViewStyle(value):
+            view.activityIndicatorViewStyle = value
+        case let .color(value):
+            view.color = value
+        }
+    }
+    
+    public static func == (lhs: ActivityIndicatorViewStyle, rhs: ActivityIndicatorViewStyle) -> Bool {
+        switch (lhs, rhs) {
+        case let (.isAnimating(left), .isAnimating(right)),
+             let (.hidesWhenStopped(left), .hidesWhenStopped(right)):
+            return left == right
+        case let (.activityIndicatorViewStyle(left), .activityIndicatorViewStyle(right)):
+            return left == right
+        case let (.color(left), .color(right)):
+            return left == right
+        default:
+            return false
+        }
+    }
+    
+    @inline(__always) static func startIndex() -> Int {
+        return UISliderStyle.lastStyleIndex()
+    }
+    
+    @inline(__always) static func stylesCount() -> Int {
+        return 4
+    }
+    
+    @inline(__always) func value() -> (index: Int, valueHash: Int) {
+        switch self {
+        case let .isAnimating(value):
+            return (1, value.hashValue)
+        case let .hidesWhenStopped(value):
+            return (2, value.hashValue)
+        case let .activityIndicatorViewStyle(value):
+            return (3, value.hashValue)
+        case let .color(value):
+            return (4, value?.hashValue ?? 0)
+        }
+    }
+}
+
+public extension Component where T: UIActivityIndicatorView {
+    public func activityIndicatorStyles(_ styles: ActivityIndicatorViewStyle...) -> Component<T> {
+        return add(styles: styles)
+    }
+}
---
+++ ViewComponentsTests/TestActivityIndicatorViewStyle.swift
@@ -0,0 +1,66 @@
+//
+//  TestActivityIndicatorViewStyle.swift
+//  ViewComponents
+//
+//  Created by Oleksii on 15/05/2017.
+//  Copyright © 2017 WeAreReasonablePeople. All rights reserved.
+//
+
+import XCTest
+import ViewComponents
+
+class TestActivityIndicatorViewStyle: XCTestCase {
+    
+    static let allStyles: [ActivityIndicatorViewStyle] = [
+        .isAnimating(true), .hidesWhenStopped(true), .activityIndicatorViewStyle(.gray),
+        .color(.gray), .color(.none)
+    ]
+    
+    static var accumulatedHashes: [Int] {
+        return TestUISliderStyle.accumulatedHashes + TestActivityIndicatorViewStyle.allStyles.map({ $0.hashValue })
+    }
+    
+    func testStyleEquatable() {
+        XCTAssertEqual(ActivityIndicatorViewStyle.isAnimating(true), .isAnimating(true))
+        XCTAssertEqual(ActivityIndicatorViewStyle.hidesWhenStopped(true), .hidesWhenStopped(true))
+        XCTAssertEqual(ActivityIndicatorViewStyle.activityIndicatorViewStyle(.gray), .activityIndicatorViewStyle(.gray))
+        XCTAssertEqual(ActivityIndicatorViewStyle.color(.gray), .color(.gray))
+        XCTAssertNotEqual(ActivityIndicatorViewStyle.color(.gray), .activityIndicatorViewStyle(.gray))
+    }
+    
+    func testStyleSideEffects() {
+        let view = UIActivityIndicatorView()
+        
+        view.stopAnimating()
+        ActivityIndicatorViewStyle.isAnimating(true).sideEffect(on: view)
+        XCTAssertEqual(view.isAnimating, true)
+        ActivityIndicatorViewStyle.isAnimating(false).sideEffect(on: view)
+        XCTAssertEqual(view.isAnimating, false)
+        
+        view.hidesWhenStopped = false
+        ActivityIndicatorViewStyle.hidesWhenStopped(true).sideEffect(on: view)
+        XCTAssertEqual(view.hidesWhenStopped, true)
+        
+        view.activityIndicatorViewStyle = .whiteLarge
+        ActivityIndicatorViewStyle.activityIndicatorViewStyle(.gray).sideEffect(on: view)
+        XCTAssertEqual(view.activityIndicatorViewStyle, .gray)
+        
+        view.color = nil
+        ActivityIndicatorViewStyle.color(.gray).sideEffect(on: view)
+        XCTAssertEqual(view.color, .gray)
+        
+        view.color = nil
+        Component<UIActivityIndicatorView>().activityIndicatorStyles(.color(.gray)).configure(view: view)
+        XCTAssertEqual(view.color, .gray)
+    }
+    
+    func testHashValue() {
+        var hashes = Set(TestUISliderStyle.accumulatedHashes)
+        
+        for item in TestActivityIndicatorViewStyle.allStyles {
+            let hash = item.hashValue
+            XCTAssertFalse(hashes.contains(hash))
+            hashes.insert(hash)
+        }
+    }
+}
