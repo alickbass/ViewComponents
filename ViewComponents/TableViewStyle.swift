d705c7c33a9b9c68eb2a442bbeaebd046e1564b8
Oleksii Dykan <alick_bass@mail.ru>
2017-06-13 17:39:25 +0200

    Refactor UITableView to Style

--- ViewComponents/TableViewStyle.swift
+++ ViewComponents/TableViewStyle.swift
@@ -8,17 +8,19 @@
 
 import UIKit
 
+public enum TableViewStyleKey: Int, Hashable {
+    case rowHeight = 113, separatorStyle, separatorColor
+    case separatorEffect, separatorInset, cellLayoutMarginsFollowReadableWidth
+    case sectionHeaderHeight, sectionFooterHeight, estimatedRowHeight
+    case estimatedSectionHeaderHeight, estimatedSectionFooterHeight, allowsSelection
+    case allowsMultipleSelection, allowsSelectionDuringEditing, allowsMultipleSelectionDuringEditing
+    case isEditing, sectionIndexColor, sectionIndexBackgroundColor
+    case sectionIndexTrackingBackgroundColor, remembersLastFocusedIndexPath, dataSource
+    case delegate
+}
+
 public enum TableViewStyle<T: UITableView>: KeyedStyle {
-    public enum Key: Int, Hashable {
-        case rowHeight = 113, separatorStyle, separatorColor
-        case separatorEffect, separatorInset, cellLayoutMarginsFollowReadableWidth
-        case sectionHeaderHeight, sectionFooterHeight, estimatedRowHeight
-        case estimatedSectionHeaderHeight, estimatedSectionFooterHeight, allowsSelection
-        case allowsMultipleSelection, allowsSelectionDuringEditing, allowsMultipleSelectionDuringEditing
-        case isEditing, sectionIndexColor, sectionIndexBackgroundColor
-        case sectionIndexTrackingBackgroundColor, remembersLastFocusedIndexPath, dataSource
-        case delegate
-    }
+    public typealias Key = TableViewStyleKey
     
     case rowHeight(CGFloat)
     case separatorStyle(UITableViewCellSeparatorStyle)
@@ -182,5 +184,97 @@ public enum TableViewStyle<T: UITableView>: KeyedStyle {
 public extension Component where T: UITableView {
     public func tableView(_ styles: TableViewStyle<T>...) -> Component<T> {
         return adding(styles: styles.lazy.map(AnyStyle.init))
+    }
+}
+
+public extension AnyStyle where T: UITableView {
+    private typealias ViewStyle<Item> = Style<T, Item, TableViewStyleKey>
+    
+    public static func rowHeight(_ value: CGFloat) -> AnyStyle<T> {
+        return ViewStyle(value, key: .rowHeight, sideEffect: { $0.rowHeight = $1 }).toAnyStyle
+    }
+    
+    public static func separatorStyle(_ value: UITableViewCellSeparatorStyle) -> AnyStyle<T> {
+        return ViewStyle(value, key: .separatorStyle, sideEffect: { $0.separatorStyle = $1 }).toAnyStyle
+    }
+    
+    public static func separatorColor(_ value: UIColor?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .separatorColor, sideEffect: { $0.separatorColor = $1 }).toAnyStyle
+    }
+    
+    public static func separatorEffect(_ value: UIVisualEffect?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .separatorEffect, sideEffect: { $0.separatorEffect = $1 }).toAnyStyle
+    }
+    
+    public static func separatorInset(_ value: UIEdgeInsets) -> AnyStyle<T> {
+        return ViewStyle(value, key: .separatorInset, sideEffect: { $0.separatorInset = $1 }).toAnyStyle
+    }
+    
+    public static func cellLayoutMarginsFollowReadableWidth(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .cellLayoutMarginsFollowReadableWidth, sideEffect: { $0.cellLayoutMarginsFollowReadableWidth = $1 }).toAnyStyle
+    }
+    
+    public static func sectionHeaderHeight(_ value: CGFloat) -> AnyStyle<T> {
+        return ViewStyle(value, key: .sectionHeaderHeight, sideEffect: { $0.sectionHeaderHeight = $1 }).toAnyStyle
+    }
+    
+    public static func sectionFooterHeight(_ value: CGFloat) -> AnyStyle<T> {
+        return ViewStyle(value, key: .sectionFooterHeight, sideEffect: { $0.sectionFooterHeight = $1 }).toAnyStyle
+    }
+    
+    public static func estimatedRowHeight(_ value: CGFloat) -> AnyStyle<T> {
+        return ViewStyle(value, key: .estimatedRowHeight, sideEffect: { $0.estimatedRowHeight = $1 }).toAnyStyle
+    }
+    
+    public static func estimatedSectionHeaderHeight(_ value: CGFloat) -> AnyStyle<T> {
+        return ViewStyle(value, key: .estimatedSectionHeaderHeight, sideEffect: { $0.estimatedSectionHeaderHeight = $1 }).toAnyStyle
+    }
+    
+    public static func estimatedSectionFooterHeight(_ value: CGFloat) -> AnyStyle<T> {
+        return ViewStyle(value, key: .estimatedSectionFooterHeight, sideEffect: { $0.estimatedSectionFooterHeight = $1 }).toAnyStyle
+    }
+    
+    public static func allowsSelection(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .allowsSelection, sideEffect: { $0.allowsSelection = $1 }).toAnyStyle
+    }
+    
+    public static func allowsMultipleSelection(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .allowsMultipleSelection, sideEffect: { $0.allowsMultipleSelection = $1 }).toAnyStyle
+    }
+    
+    public static func allowsSelectionDuringEditing(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .allowsSelectionDuringEditing, sideEffect: { $0.allowsSelectionDuringEditing = $1 }).toAnyStyle
+    }
+    
+    public static func allowsMultipleSelectionDuringEditing(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .allowsMultipleSelectionDuringEditing, sideEffect: { $0.allowsMultipleSelectionDuringEditing = $1 }).toAnyStyle
+    }
+    
+    public static func isEditing(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .isEditing, sideEffect: { $0.isEditing = $1 }).toAnyStyle
+    }
+    
+    public static func sectionIndexColor(_ value: UIColor?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .sectionIndexColor, sideEffect: { $0.sectionIndexColor = $1 }).toAnyStyle
+    }
+    
+    public static func sectionIndexBackgroundColor(_ value: UIColor?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .sectionIndexBackgroundColor, sideEffect: { $0.sectionIndexBackgroundColor = $1 }).toAnyStyle
+    }
+    
+    public static func sectionIndexTrackingBackgroundColor(_ value: UIColor?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .sectionIndexTrackingBackgroundColor, sideEffect: { $0.sectionIndexTrackingBackgroundColor = $1 }).toAnyStyle
+    }
+    
+    public static func remembersLastFocusedIndexPath(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .remembersLastFocusedIndexPath, sideEffect: { $0.remembersLastFocusedIndexPath = $1 }).toAnyStyle
+    }
+    
+    public static func dataSource(_ value: UITableViewDataSource?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .dataSource, sideEffect: { $0.dataSource = $1 }, equality: { $0 === $1 }, hash: { $0?.hash ?? 0 }).toAnyStyle
+    }
+    
+    public static func delegate(_ value: UITableViewDelegate?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .delegate, sideEffect: { $0.delegate = $1 }, equality: { $0 === $1 }, hash: { $0?.hash ?? 0 }).toAnyStyle
     }
 }
--- ViewComponentsTests/TestTableViewStyle.swift
+++ ViewComponentsTests/TestTableViewStyle.swift
@@ -63,6 +63,11 @@ class TestTableViewStyle: XCTestCase, ViewTestType {
         XCTAssertNotEqual(TableViewStyle.remembersLastFocusedIndexPath(true), .sectionIndexTrackingBackgroundColor(.red))
     }
     
+    func testAnyStyleEquatable() {
+        XCTAssertEqual(AnyStyle<UITableView>.dataSource(controller), .dataSource(controller))
+        XCTAssertEqual(AnyStyle<UITableView>.delegate(controller), .delegate(controller))
+    }
+    
     func testStyleSideEffects() {
         let view = UITableView()
         
@@ -152,6 +157,102 @@ class TestTableViewStyle: XCTestCase, ViewTestType {
         
         view.delegate = nil
         TableViewStyle.delegate(controller).sideEffect(on: view)
+        XCTAssertTrue(view.delegate === controller)
+        
+        view.remembersLastFocusedIndexPath = false
+        Component<UITableView>().tableView(.remembersLastFocusedIndexPath(true)).configure(item: view)
+        XCTAssertEqual(view.remembersLastFocusedIndexPath, true)
+    }
+    
+    func testAnyStyleSideEffects() {
+        let view = UITableView()
+        
+        view.rowHeight = 50
+        AnyStyle<UITableView>.rowHeight(40).sideEffect(on: view)
+        XCTAssertEqual(view.rowHeight, 40)
+        
+        view.separatorStyle = .none
+        AnyStyle<UITableView>.separatorStyle(.singleLine).sideEffect(on: view)
+        XCTAssertEqual(view.separatorStyle, .singleLine)
+        
+        view.separatorColor = nil
+        AnyStyle<UITableView>.separatorColor(.red).sideEffect(on: view)
+        XCTAssertEqual(view.separatorColor, .red)
+        
+        view.separatorEffect = nil
+        AnyStyle<UITableView>.separatorEffect(blur).sideEffect(on: view)
+        XCTAssertNotNil(view.separatorEffect)
+        
+        view.separatorInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        AnyStyle<UITableView>.separatorInset(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.separatorInset, .zero)
+        
+        view.cellLayoutMarginsFollowReadableWidth = false
+        AnyStyle<UITableView>.cellLayoutMarginsFollowReadableWidth(true).sideEffect(on: view)
+        XCTAssertEqual(view.cellLayoutMarginsFollowReadableWidth, true)
+        
+        view.sectionHeaderHeight = 30
+        AnyStyle<UITableView>.sectionHeaderHeight(40).sideEffect(on: view)
+        XCTAssertEqual(view.sectionHeaderHeight, 40)
+        
+        view.sectionFooterHeight = 30
+        AnyStyle<UITableView>.sectionFooterHeight(40).sideEffect(on: view)
+        XCTAssertEqual(view.sectionFooterHeight, 40)
+        
+        view.estimatedRowHeight = 30
+        AnyStyle<UITableView>.estimatedRowHeight(40).sideEffect(on: view)
+        XCTAssertEqual(view.estimatedRowHeight, 40)
+        
+        view.estimatedSectionHeaderHeight = 30
+        AnyStyle<UITableView>.estimatedSectionHeaderHeight(40).sideEffect(on: view)
+        XCTAssertEqual(view.estimatedSectionHeaderHeight, 40)
+        
+        view.estimatedSectionFooterHeight = 30
+        AnyStyle<UITableView>.estimatedSectionFooterHeight(40).sideEffect(on: view)
+        XCTAssertEqual(view.estimatedSectionFooterHeight, 40)
+        
+        view.allowsSelection = false
+        AnyStyle<UITableView>.allowsSelection(true).sideEffect(on: view)
+        XCTAssertEqual(view.allowsSelection, true)
+        
+        view.allowsMultipleSelection = false
+        AnyStyle<UITableView>.allowsMultipleSelection(true).sideEffect(on: view)
+        XCTAssertEqual(view.allowsMultipleSelection, true)
+        
+        view.allowsSelectionDuringEditing = false
+        AnyStyle<UITableView>.allowsSelectionDuringEditing(true).sideEffect(on: view)
+        XCTAssertEqual(view.allowsSelectionDuringEditing, true)
+        
+        view.allowsMultipleSelectionDuringEditing = false
+        AnyStyle<UITableView>.allowsMultipleSelectionDuringEditing(true).sideEffect(on: view)
+        XCTAssertEqual(view.allowsMultipleSelectionDuringEditing, true)
+        
+        view.isEditing = false
+        AnyStyle<UITableView>.isEditing(true).sideEffect(on: view)
+        XCTAssertEqual(view.isEditing, true)
+        
+        view.sectionIndexColor = nil
+        AnyStyle<UITableView>.sectionIndexColor(.red).sideEffect(on: view)
+        XCTAssertEqual(view.sectionIndexColor, .red)
+        
+        view.sectionIndexBackgroundColor = nil
+        AnyStyle<UITableView>.sectionIndexBackgroundColor(.red).sideEffect(on: view)
+        XCTAssertEqual(view.sectionIndexBackgroundColor, .red)
+        
+        view.sectionIndexTrackingBackgroundColor = nil
+        AnyStyle<UITableView>.sectionIndexTrackingBackgroundColor(.red).sideEffect(on: view)
+        XCTAssertEqual(view.sectionIndexTrackingBackgroundColor, .red)
+        
+        view.remembersLastFocusedIndexPath = false
+        AnyStyle<UITableView>.remembersLastFocusedIndexPath(true).sideEffect(on: view)
+        XCTAssertEqual(view.remembersLastFocusedIndexPath, true)
+        
+        view.dataSource = nil
+        AnyStyle<UITableView>.dataSource(controller).sideEffect(on: view)
+        XCTAssertTrue(view.dataSource === controller)
+        
+        view.delegate = nil
+        AnyStyle<UITableView>.delegate(controller).sideEffect(on: view)
         XCTAssertTrue(view.delegate === controller)
         
         view.remembersLastFocusedIndexPath = false
