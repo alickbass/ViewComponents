66bb3b585c5a2d78207729ca8d9a5ecd4ef4ef8a
Oleksii Dykan <alick_bass@mail.ru>
2017-06-13 17:51:19 +0200

    Refactor tableviewcell style to Style

--- ViewComponents/TableViewCellStyle.swift
+++ ViewComponents/TableViewCellStyle.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
+public enum TableViewCellStyleKey: Int, Hashable {
+    case accessoryType = 135, editingAccessoryType, isSelected
+    case selectionStyle, isHighlighted, isEditing
+    case showsReorderControl, indentationLevel, indentationWidth
+    case shouldIndentWhileEditing, separatorInset, focusStyle
+}
+
 public enum TableViewCellStyle<T: UITableViewCell>: KeyedStyle {
-    public enum Key: Int, Hashable {
-        case accessoryType = 135, editingAccessoryType, isSelected
-        case selectionStyle, isHighlighted, isEditing
-        case showsReorderControl, indentationLevel, indentationWidth
-        case shouldIndentWhileEditing, separatorInset, focusStyle
-    }
+    public typealias Key = TableViewCellStyleKey
     
     case accessoryType(UITableViewCellAccessoryType)
     case editingAccessoryType(UITableViewCellAccessoryType)
@@ -117,5 +119,57 @@ public enum TableViewCellStyle<T: UITableViewCell>: KeyedStyle {
 public extension Component where T: UITableViewCell {
     public func tableViewCell(_ styles: TableViewCellStyle<T>...) -> Component<T> {
         return adding(styles: styles.lazy.map(AnyStyle.init))
+    }
+}
+
+public extension AnyStyle where T: UITableViewCell {
+    private typealias ViewStyle<Item> = Style<T, Item, TableViewCellStyleKey>
+    
+    public static func accessoryType(_ value: UITableViewCellAccessoryType) -> AnyStyle<T> {
+        return ViewStyle(value, key: .accessoryType, sideEffect: { $0.accessoryType = $1 }).toAnyStyle
+    }
+    
+    public static func editingAccessoryType(_ value: UITableViewCellAccessoryType) -> AnyStyle<T> {
+        return ViewStyle(value, key: .editingAccessoryType, sideEffect: { $0.editingAccessoryType = $1 }).toAnyStyle
+    }
+    
+    public static func isSelected(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .isSelected, sideEffect: { $0.isSelected = $1 }).toAnyStyle
+    }
+    
+    public static func selectionStyle(_ value: UITableViewCellSelectionStyle) -> AnyStyle<T> {
+        return ViewStyle(value, key: .selectionStyle, sideEffect: { $0.selectionStyle = $1 }).toAnyStyle
+    }
+    
+    public static func isHighlighted(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .isHighlighted, sideEffect: { $0.isHighlighted = $1 }).toAnyStyle
+    }
+    
+    public static func isEditing(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .isEditing, sideEffect: { $0.isEditing = $1 }).toAnyStyle
+    }
+    
+    public static func showsReorderControl(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .showsReorderControl, sideEffect: { $0.showsReorderControl = $1 }).toAnyStyle
+    }
+    
+    public static func indentationLevel(_ value: Int) -> AnyStyle<T> {
+        return ViewStyle(value, key: .indentationLevel, sideEffect: { $0.indentationLevel = $1 }).toAnyStyle
+    }
+    
+    public static func indentationWidth(_ value: CGFloat) -> AnyStyle<T> {
+        return ViewStyle(value, key: .indentationWidth, sideEffect: { $0.indentationWidth = $1 }).toAnyStyle
+    }
+    
+    public static func shouldIndentWhileEditing(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .shouldIndentWhileEditing, sideEffect: { $0.shouldIndentWhileEditing = $1 }).toAnyStyle
+    }
+    
+    public static func separatorInset(_ value: UIEdgeInsets) -> AnyStyle<T> {
+        return ViewStyle(value, key: .separatorInset, sideEffect: { $0.separatorInset = $1 }).toAnyStyle
+    }
+    
+    public static func focusStyle(_ value: UITableViewCellFocusStyle) -> AnyStyle<T> {
+        return ViewStyle(value, key: .focusStyle, sideEffect: { $0.focusStyle = $1 }).toAnyStyle
     }
 }
--- ViewComponentsTests/TestTableViewCellStyle.swift
+++ ViewComponentsTests/TestTableViewCellStyle.swift
@@ -93,6 +93,54 @@ class TestTableViewCellStyle: XCTestCase, ViewTestType {
         XCTAssertEqual(view.showsReorderControl, true)
     }
     
+    func testAnyStyleSideEffects() {
+        let view = UITableViewCell()
+        
+        view.accessoryType = .checkmark
+        AnyStyle<UITableViewCell>.accessoryType(.detailButton).sideEffect(on: view)
+        XCTAssertEqual(view.accessoryType, .detailButton)
+        
+        view.editingAccessoryType = .checkmark
+        AnyStyle<UITableViewCell>.editingAccessoryType(.detailButton).sideEffect(on: view)
+        XCTAssertEqual(view.editingAccessoryType, .detailButton)
+        
+        view.isSelected = false
+        AnyStyle<UITableViewCell>.isSelected(true).sideEffect(on: view)
+        XCTAssertEqual(view.isSelected, true)
+        
+        view.selectionStyle = .blue
+        AnyStyle<UITableViewCell>.selectionStyle(.gray).sideEffect(on: view)
+        XCTAssertEqual(view.selectionStyle, .gray)
+        
+        view.isHighlighted = false
+        AnyStyle<UITableViewCell>.isHighlighted(true).sideEffect(on: view)
+        XCTAssertEqual(view.isHighlighted, true)
+        
+        view.isEditing = false
+        AnyStyle<UITableViewCell>.isEditing(true).sideEffect(on: view)
+        XCTAssertEqual(view.isEditing, true)
+        
+        view.showsReorderControl = false
+        AnyStyle<UITableViewCell>.showsReorderControl(true).sideEffect(on: view)
+        XCTAssertEqual(view.showsReorderControl, true)
+        
+        view.indentationLevel = 2
+        AnyStyle<UITableViewCell>.indentationLevel(3).sideEffect(on: view)
+        XCTAssertEqual(view.indentationLevel, 3)
+        
+        view.indentationWidth = 2
+        AnyStyle<UITableViewCell>.indentationWidth(3).sideEffect(on: view)
+        XCTAssertEqual(view.indentationWidth, 3)
+        
+        view.shouldIndentWhileEditing = false
+        AnyStyle<UITableViewCell>.shouldIndentWhileEditing(true).sideEffect(on: view)
+        XCTAssertEqual(view.shouldIndentWhileEditing, true)
+        
+        view.separatorInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        AnyStyle<UITableViewCell>.separatorInset(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.separatorInset, .zero)
+    }
+    
     func testHashValue() {
         testAccumulatingHashes()
     }
