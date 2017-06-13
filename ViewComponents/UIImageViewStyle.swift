89fce20615f2343c0772af4349e249c6bfc00150
Oleksii Dykan <alick_bass@mail.ru>
2017-06-13 23:59:51 +0200

    Remove UIImageViewStyle

--- ViewComponents/UIImageViewStyle.swift
+++ ViewComponents/UIImageViewStyle.swift
@@ -8,94 +8,10 @@
 
 import UIKit
 
-public enum UIImageViewStyleKey: Int, Hashable {
+private enum UIImageViewStyleKey: Int, Hashable {
     case image = 78, highlightedImage, animationImages
     case highlightedAnimationImages, animationDuration, animationRepeatCount
     case isHighlighted
-}
-
-public enum UIImageViewStyle<T: UIImageView>: KeyedStyle {
-    public typealias Key = UIImageViewStyleKey
-    
-    case image(UIImage?)
-    case highlightedImage(UIImage?)
-    case animationImages([UIImage]?)
-    case highlightedAnimationImages([UIImage]?)
-    case animationDuration(TimeInterval)
-    case animationRepeatCount(Int)
-    case isHighlighted(Bool)
-    
-    public func sideEffect(on view: T) {
-        switch self {
-        case let .image(image):
-            view.image = image
-        case let .highlightedImage(image):
-            view.highlightedImage = image
-        case let .animationImages(images):
-            view.animationImages = images
-        case let .highlightedAnimationImages(images):
-            view.highlightedAnimationImages = images
-        case let .animationDuration(duration):
-            view.animationDuration = duration
-        case let .animationRepeatCount(animationRepeatCount):
-            view.animationRepeatCount = animationRepeatCount
-        case let .isHighlighted(isHighlighted):
-            view.isHighlighted = isHighlighted
-        }
-    }
-    
-    public static func == (lhs: UIImageViewStyle<T>, rhs: UIImageViewStyle<T>) -> Bool {
-        switch (lhs, rhs) {
-        case let (.image(leftImage), .image(rightImage)),
-             let (.highlightedImage(leftImage), .highlightedImage(rightImage)):
-            return leftImage == rightImage
-        case let (.animationImages(leftImages), .animationImages(rightImages)),
-             let (.highlightedAnimationImages(leftImages), .highlightedAnimationImages(rightImages)):
-            switch (leftImages, rightImages) {
-            case (nil, nil):
-                return true
-            case let (left?, right?):
-                return left == right
-            default:
-                return false
-            }
-        case let (.animationDuration(leftDuration), .animationDuration(rightDuration)):
-            return leftDuration == rightDuration
-        case let (.animationRepeatCount(leftCount), .animationRepeatCount(rightCount)):
-            return leftCount == rightCount
-        case let (.isHighlighted(leftBool), .isHighlighted(rightBool)):
-            return leftBool == rightBool
-        default:
-            return false
-        }
-    }
-    
-    @inline(__always) public func value() -> (key: Key, valueHash: Int) {
-        switch self {
-        case let .image(image):
-            return (.image, image?.hashValue ?? 0)
-        case let .highlightedImage(image):
-            return (.highlightedImage, image?.hashValue ?? 0)
-        case let .animationImages(images):
-            let hash = images.map({ $0.reduce(5381, { (($0.0 << 5) &+ $0.0) &+ $0.1.hashValue }) })
-            return (.animationImages, hash ?? 0)
-        case let .highlightedAnimationImages(images):
-            let hash = images.map({ $0.reduce(5381, { (($0.0 << 5) &+ $0.0) &+ $0.1.hashValue }) })
-            return (.highlightedAnimationImages, hash ?? 0)
-        case let .animationDuration(duration):
-            return (.animationDuration, duration.hashValue)
-        case let .animationRepeatCount(animationRepeatCount):
-            return (.animationRepeatCount, animationRepeatCount.hashValue)
-        case let .isHighlighted(isHighlighted):
-            return (.isHighlighted, isHighlighted.hashValue)
-        }
-    }
-}
-
-public extension Component where T: UIImageView {
-    public func imageView(_ styles: UIImageViewStyle<T>...) -> Component<T> {
-        return adding(styles: styles.lazy.map(AnyStyle.init))
-    }
 }
 
 public extension AnyStyle where T: UIImageView {
--- ViewComponentsTests/TestUIImageViewStyle.swift
+++ ViewComponentsTests/TestUIImageViewStyle.swift
@@ -22,32 +22,6 @@ class TestUIImageViewStyle: XCTestCase, ViewTestType {
     
     static var previousHashes: [Int : Any] { return TestUIButtonStyle.accumulatedHashes }
     
-    func testStyleEquatable() {
-        XCTAssertEqual(UIImageViewStyle.image(image), .image(image))
-        XCTAssertNotEqual(UIImageViewStyle.image(image), .image(nil))
-        
-        XCTAssertEqual(UIImageViewStyle.highlightedImage(image), .highlightedImage(image))
-        XCTAssertNotEqual(UIImageViewStyle.highlightedImage(image), .highlightedImage(nil))
-        
-        XCTAssertEqual(UIImageViewStyle.animationImages([image]), .animationImages([image]))
-        XCTAssertEqual(UIImageViewStyle.animationImages(nil), .animationImages(nil))
-        XCTAssertNotEqual(UIImageViewStyle.animationImages([image]), .animationImages(nil))
-        
-        XCTAssertEqual(UIImageViewStyle.highlightedAnimationImages([image]), .highlightedAnimationImages([image]))
-        XCTAssertEqual(UIImageViewStyle.highlightedAnimationImages(nil), .highlightedAnimationImages(nil))
-        XCTAssertNotEqual(UIImageViewStyle.highlightedAnimationImages([image]), .highlightedAnimationImages(nil))
-        
-        XCTAssertEqual(UIImageViewStyle.animationDuration(12), .animationDuration(12))
-        XCTAssertNotEqual(UIImageViewStyle.animationDuration(12), .animationDuration(20))
-        
-        XCTAssertEqual(UIImageViewStyle.animationRepeatCount(12), .animationRepeatCount(12))
-        XCTAssertNotEqual(UIImageViewStyle.animationRepeatCount(12), .animationRepeatCount(20))
-        
-        XCTAssertEqual(UIImageViewStyle.isHighlighted(true), .isHighlighted(true))
-        XCTAssertNotEqual(UIImageViewStyle.isHighlighted(true), .isHighlighted(false))
-        XCTAssertNotEqual(UIImageViewStyle.isHighlighted(true), .animationRepeatCount(20))
-    }
-    
     func testAnyStyleEquatable() {
         XCTAssertEqual(AnyStyle<UIImageView>.animationImages([image]), .animationImages([image]))
         XCTAssertEqual(AnyStyle<UIImageView>.animationImages(nil), .animationImages(nil))
@@ -56,42 +30,6 @@ class TestUIImageViewStyle: XCTestCase, ViewTestType {
         XCTAssertEqual(AnyStyle<UIImageView>.highlightedAnimationImages([image]), .highlightedAnimationImages([image]))
         XCTAssertEqual(AnyStyle<UIImageView>.highlightedAnimationImages(nil), .highlightedAnimationImages(nil))
         XCTAssertNotEqual(AnyStyle<UIImageView>.highlightedAnimationImages([image]), .highlightedAnimationImages(nil))
-    }
-    
-    func testStyleSideEffects() {
-        let view = UIImageView()
-        
-        view.image = image
-        UIImageViewStyle.image(nil).sideEffect(on: view)
-        XCTAssertNil(view.image)
-        
-        view.highlightedImage = image
-        UIImageViewStyle.highlightedImage(nil).sideEffect(on: view)
-        XCTAssertNil(view.highlightedImage)
-        
-        view.animationImages = [image]
-        UIImageViewStyle.animationImages(nil).sideEffect(on: view)
-        XCTAssertNil(view.animationImages)
-        
-        view.highlightedAnimationImages = [image]
-        UIImageViewStyle.highlightedAnimationImages(nil).sideEffect(on: view)
-        XCTAssertNil(view.highlightedAnimationImages)
-        
-        view.animationDuration = 50
-        UIImageViewStyle.animationDuration(12).sideEffect(on: view)
-        XCTAssertEqual(view.animationDuration, 12)
-        
-        view.animationRepeatCount = 50
-        UIImageViewStyle.animationRepeatCount(12).sideEffect(on: view)
-        XCTAssertEqual(view.animationRepeatCount, 12)
-        
-        view.isHighlighted = false
-        UIImageViewStyle.isHighlighted(true).sideEffect(on: view)
-        XCTAssertTrue(view.isHighlighted)
-        
-        view.isHighlighted = false
-        Component<UIImageView>().imageView(.isHighlighted(true)).configure(item: view)
-        XCTAssertEqual(view.isHighlighted, true)
     }
     
     func testAnyStyleSideEffects() {
@@ -124,10 +62,6 @@ class TestUIImageViewStyle: XCTestCase, ViewTestType {
         view.isHighlighted = false
         AnyStyle<UIImageView>.isHighlighted(true).sideEffect(on: view)
         XCTAssertTrue(view.isHighlighted)
-        
-        view.isHighlighted = false
-        Component<UIImageView>().imageView(.isHighlighted(true)).configure(item: view)
-        XCTAssertEqual(view.isHighlighted, true)
     }
     
     func testHashValue() {
