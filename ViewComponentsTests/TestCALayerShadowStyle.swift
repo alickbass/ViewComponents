fbdb0d7308c8d050cc8840182e5880f2b7517e4a
Oleksii Dykan <alick_bass@mail.ru>
2017-06-13 13:37:10 +0200

    Cover calayer shadow with unit tests

--- ViewComponentsTests/TestCALayerShadowStyle.swift
+++ ViewComponentsTests/TestCALayerShadowStyle.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-import ViewComponents
+@testable import ViewComponents
 
 class TestCALayerShadowStyle: XCTestCase, ViewTestType {
     static let allStyles: [CALayer.ShadowStyle<CALayer>] = [
@@ -59,6 +59,33 @@ class TestCALayerShadowStyle: XCTestCase, ViewTestType {
         theView.layer.name = nil
         Component<UIView>().shadow(.color(.red)).configure(item: theView)
         XCTAssertEqual(view.shadowColor, UIColor.red.cgColor)
+    }
+    
+    func testLayerShadowEquatable() {
+        let shadow = LayerShadow(opacity: 0.2, radius: 0.2, offset: .zero, color: nil, path: nil)
+        let path = CGPath(rect: .zero, transform: nil)
+        XCTAssertEqual(shadow, shadow)
+        XCTAssertEqual(shadow.hashValue, shadow.hashValue)
+        XCTAssertNotEqual(LayerShadow(opacity: 0.2, radius: 0.2, offset: .zero, color: .red, path: path).hashValue, shadow.hashValue)
+    }
+    
+    func testAnyStyleSideEffect() {
+        let layer = CALayer()
+        let path = CGPath(rect: .zero, transform: nil)
+        
+        AnyStyle<CALayer>.shadow(opacity: 0.2, radius: 5, offset: CGSize(width: 5, height: 5), color: .red, path: path).sideEffect(on: layer)
+        XCTAssertEqual(layer.shadowOpacity, 0.2)
+        XCTAssertEqual(layer.shadowOffset, CGSize(width: 5, height: 5))
+        XCTAssertEqual(layer.shadowColor, UIColor.red.cgColor)
+        XCTAssertEqual(layer.shadowPath, path)
+        
+        let view = UIView()
+        
+        AnyStyle<UIView>.shadow(opacity: 0.2, radius: 5, offset: CGSize(width: 5, height: 5), color: .red, path: path).sideEffect(on: view)
+        XCTAssertEqual(view.layer.shadowOpacity, 0.2)
+        XCTAssertEqual(view.layer.shadowOffset, CGSize(width: 5, height: 5))
+        XCTAssertEqual(view.layer.shadowColor, UIColor.red.cgColor)
+        XCTAssertEqual(view.layer.shadowPath, path)
     }
     
     func testHashValue() {
