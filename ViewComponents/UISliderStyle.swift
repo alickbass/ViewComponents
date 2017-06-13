20067c6c752bbe11cb84175c7359c2a337a7d60e
Oleksii Dykan <alick_bass@mail.ru>
2017-06-13 19:20:41 +0200

    Refactor slider to Style

--- ViewComponents/UISliderStyle.swift
+++ ViewComponents/UISliderStyle.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
+public enum UISliderStyleKey: Int, Hashable {
+    case currentValue = 189, minimumValue, maximumValue
+    case isContinuous, minimumValueImage, maximumValueImage
+    case minimumTrackTintColor, minimumTrackImage, maximumTrackTintColor
+    case maximumTrackImage, thumbTintColor, thumbImage
+}
+
 public enum UISliderStyle<T: UISlider>: KeyedStyle {
-    public enum Key: Int, Hashable {
-        case currentValue = 189, minimumValue, maximumValue
-        case isContinuous, minimumValueImage, maximumValueImage
-        case minimumTrackTintColor, minimumTrackImage, maximumTrackTintColor
-        case maximumTrackImage, thumbTintColor, thumbImage
-    }
+    public typealias Key = UISliderStyleKey
     
     case currentValue(Float, animated: Bool)
     case minimumValue(Float)
@@ -128,5 +130,69 @@ public enum UISliderStyle<T: UISlider>: KeyedStyle {
 public extension Component where T: UISlider {
     public func slider(_ styles: UISliderStyle<T>...) -> Component<T> {
         return adding(styles: styles.lazy.map(AnyStyle.init))
+    }
+}
+
+public extension AnyStyle where T: UISlider {
+    private typealias ViewStyle<Item> = Style<T, Item, UISliderStyleKey>
+    
+    public static func currentValue(_ value: Float, animated: Bool) -> AnyStyle<T> {
+        return ViewStyle<(Float, Bool)>((value, animated), key: .currentValue,
+            sideEffect: { $0.setValue($1.0, animated: $1.1) },
+            equality: { $0.0 == $1.0 && $0.1 == $1.1 },
+            hash: { value -> Int in
+                var hash = 5381
+                hash = ((hash << 5) &+ hash) &+ value.0.hashValue
+                hash = ((hash << 5) &+ hash) &+ value.1.hashValue
+                return hash
+            }
+        ).toAnyStyle
+    }
+    
+    public static func minimumValue(_ value: Float) -> AnyStyle<T> {
+        return ViewStyle(value, key: .minimumValue, sideEffect: { $0.minimumValue = $1 }).toAnyStyle
+    }
+    
+    public static func maximumValue(_ value: Float) -> AnyStyle<T> {
+        return ViewStyle(value, key: .maximumValue, sideEffect: { $0.maximumValue = $1 }).toAnyStyle
+    }
+    
+    public static func isContinuous(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .isContinuous, sideEffect: { $0.isContinuous = $1 }).toAnyStyle
+    }
+    
+    public static func minimumValueImage(_ value: UIImage?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .minimumValueImage, sideEffect: { $0.minimumValueImage = $1 }).toAnyStyle
+    }
+    
+    public static func maximumValueImage(_ value: UIImage?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .maximumValueImage, sideEffect: { $0.maximumValueImage = $1 }).toAnyStyle
+    }
+    
+    public static func minimumTrackTintColor(_ value: UIColor?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .minimumTrackTintColor, sideEffect: { $0.minimumTrackTintColor = $1 }).toAnyStyle
+    }
+    
+    public static func minimumTrackImage(_ value: UIImage?, for state: UIControlState = .normal) -> AnyStyle<T> {
+        let input = StateInput(value: value, state: state)
+        return ViewStyle(input, key: .minimumTrackImage, sideEffect: { $0.setMinimumTrackImage($1.value, for: $1.state) }).toAnyStyle
+    }
+    
+    public static func maximumTrackTintColor(_ value: UIColor?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .maximumTrackTintColor, sideEffect: { $0.maximumTrackTintColor = $1 }).toAnyStyle
+    }
+    
+    public static func maximumTrackImage(_ value: UIImage?, for state: UIControlState = .normal) -> AnyStyle<T> {
+        let input = StateInput(value: value, state: state)
+        return ViewStyle(input, key: .maximumTrackImage, sideEffect: { $0.setMaximumTrackImage($1.value, for: $1.state) }).toAnyStyle
+    }
+    
+    public static func thumbTintColor(_ value: UIColor?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .thumbTintColor, sideEffect: { $0.thumbTintColor = $1 }).toAnyStyle
+    }
+    
+    public static func thumbImage(_ value: UIImage?, for state: UIControlState = .normal) -> AnyStyle<T> {
+        let input = StateInput(value: value, state: state)
+        return ViewStyle(input, key: .thumbImage, sideEffect: { $0.setThumbImage($1.value, for: $1.state) }).toAnyStyle
     }
 }
--- ViewComponentsTests/TestUISliderStyle.swift
+++ ViewComponentsTests/TestUISliderStyle.swift
@@ -40,6 +40,10 @@ class TestUISliderStyle: XCTestCase, ViewTestType {
         XCTAssertNotEqual(UISliderStyle.thumbImage(image, for: .normal), .thumbTintColor(.blue))
     }
     
+    func testAnyStyleEquatable() {
+        XCTAssertEqual(AnyStyle<UISlider>.currentValue(0.2, animated: true), .currentValue(0.2, animated: true))
+    }
+    
     func testStyleSideEffects() {
         let view = UISlider()
         
@@ -89,6 +93,62 @@ class TestUISliderStyle: XCTestCase, ViewTestType {
         
         view.setThumbImage(nil, for: .normal)
         UISliderStyle.thumbImage(image, for: .normal).sideEffect(on: view)
+        XCTAssertEqual(view.thumbImage(for: .normal), image)
+        
+        view.minimumValue = 0.0
+        Component<UISlider>().slider(.minimumValue(0.2)).configure(item: view)
+        XCTAssertEqual(view.minimumValue, 0.2)
+    }
+    
+    func testAnyStyleSideEffects() {
+        let view = UISlider()
+        
+        view.setValue(0.6, animated: false)
+        AnyStyle<UISlider>.currentValue(0.2, animated: false).sideEffect(on: view)
+        XCTAssertEqual(view.value, 0.2)
+        
+        view.minimumValue = 0.0
+        AnyStyle<UISlider>.minimumValue(0.2).sideEffect(on: view)
+        XCTAssertEqual(view.minimumValue, 0.2)
+        
+        view.maximumValue = 0.5
+        AnyStyle<UISlider>.maximumValue(0.8).sideEffect(on: view)
+        XCTAssertEqual(view.maximumValue, 0.8)
+        
+        view.isContinuous = false
+        AnyStyle<UISlider>.isContinuous(true).sideEffect(on: view)
+        XCTAssertEqual(view.isContinuous, true)
+        
+        view.minimumValueImage = nil
+        AnyStyle<UISlider>.minimumValueImage(image).sideEffect(on: view)
+        XCTAssertEqual(view.minimumValueImage, image)
+        
+        view.maximumValueImage = nil
+        AnyStyle<UISlider>.maximumValueImage(image).sideEffect(on: view)
+        XCTAssertEqual(view.maximumValueImage, image)
+        
+        view.minimumTrackTintColor = .red
+        AnyStyle<UISlider>.minimumTrackTintColor(.blue).sideEffect(on: view)
+        XCTAssertEqual(view.minimumTrackTintColor, .blue)
+        
+        view.maximumTrackTintColor = .red
+        AnyStyle<UISlider>.maximumTrackTintColor(.blue).sideEffect(on: view)
+        XCTAssertEqual(view.maximumTrackTintColor, .blue)
+        
+        view.thumbTintColor = .red
+        AnyStyle<UISlider>.thumbTintColor(.blue).sideEffect(on: view)
+        XCTAssertEqual(view.thumbTintColor, .blue)
+        
+        view.setMinimumTrackImage(nil, for: .normal)
+        AnyStyle<UISlider>.minimumTrackImage(image, for: .normal).sideEffect(on: view)
+        XCTAssertEqual(view.minimumTrackImage(for: .normal), image)
+        
+        view.setMaximumTrackImage(nil, for: .normal)
+        AnyStyle<UISlider>.maximumTrackImage(image, for: .normal).sideEffect(on: view)
+        XCTAssertEqual(view.maximumTrackImage(for: .normal), image)
+        
+        view.setThumbImage(nil, for: .normal)
+        AnyStyle<UISlider>.thumbImage(image, for: .normal).sideEffect(on: view)
         XCTAssertEqual(view.thumbImage(for: .normal), image)
         
         view.minimumValue = 0.0
