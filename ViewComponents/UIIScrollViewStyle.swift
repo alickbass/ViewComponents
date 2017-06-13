a5369f45ed5652143e04475c7e618c19cab6b403
Oleksii Dykan <alick_bass@mail.ru>
2017-06-13 17:02:12 +0200

    Refactor scrollview style to use style

--- ViewComponents/UIIScrollViewStyle.swift
+++ ViewComponents/UIIScrollViewStyle.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 
+public enum UIScrollViewStyleKey: Int, Hashable {
+    case contentOffset = 85, contentSize, contentInset
+    case isScrollEnabled, isDirectionalLockEnabled, isPagingEnabled
+    case bounces, alwaysBounceVertical, alwaysBounceHorizontal
+    case canCancelContentTouches, delaysContentTouches, indicatorStyle
+    case scrollIndicatorInsets, showsHorizontalScrollIndicator, showsVerticalScrollIndicator
+    case maximumZoomScale, minimumZoomScale, bouncesZoom
+}
+
 public enum UIScrollViewStyle<T: UIScrollView>: KeyedStyle {
-    public enum Key: Int, Hashable {
-        case contentOffset = 85, contentSize, contentInset
-        case isScrollEnabled, isDirectionalLockEnabled, isPagingEnabled
-        case bounces, alwaysBounceVertical, alwaysBounceHorizontal
-        case canCancelContentTouches, delaysContentTouches, indicatorStyle
-        case scrollIndicatorInsets, showsHorizontalScrollIndicator, showsVerticalScrollIndicator
-        case maximumZoomScale, minimumZoomScale, bouncesZoom
-    }
+    public typealias Key = UIScrollViewStyleKey
     
     case contentOffset(CGPoint)
     case contentSize(CGSize)
@@ -154,5 +156,81 @@ public enum UIScrollViewStyle<T: UIScrollView>: KeyedStyle {
 public extension Component where T: UIScrollView {
     public func scrollView(_ styles: UIScrollViewStyle<T>...) -> Component<T> {
         return adding(styles: styles.lazy.map(AnyStyle.init))
+    }
+}
+
+public extension AnyStyle where T: UIScrollView {
+    private typealias ViewStyle<Item> = Style<T, Item, UIScrollViewStyleKey>
+    
+    public static func contentOffset(_ value: CGPoint) -> AnyStyle<T> {
+        return ViewStyle(value, key: .contentOffset, sideEffect: { $0.contentOffset = $1 }).toAnyStyle
+    }
+    
+    public static func contentSize(_ value: CGSize) -> AnyStyle<T> {
+        return ViewStyle(value, key: .contentSize, sideEffect: { $0.contentSize = $1 }).toAnyStyle
+    }
+    
+    public static func contentInset(_ value: UIEdgeInsets) -> AnyStyle<T> {
+        return ViewStyle(value, key: .contentInset, sideEffect: { $0.contentInset = $1 }).toAnyStyle
+    }
+    
+    public static func isScrollEnabled(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .isScrollEnabled, sideEffect: { $0.isScrollEnabled = $1 }).toAnyStyle
+    }
+    
+    public static func isDirectionalLockEnabled(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .isDirectionalLockEnabled, sideEffect: { $0.isDirectionalLockEnabled = $1 }).toAnyStyle
+    }
+    
+    public static func isPagingEnabled(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .isPagingEnabled, sideEffect: { $0.isPagingEnabled = $1 }).toAnyStyle
+    }
+    
+    public static func bounces(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .bounces, sideEffect: { $0.bounces = $1 }).toAnyStyle
+    }
+    
+    public static func alwaysBounceVertical(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .alwaysBounceVertical, sideEffect: { $0.alwaysBounceVertical = $1 }).toAnyStyle
+    }
+    
+    public static func alwaysBounceHorizontal(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .alwaysBounceHorizontal, sideEffect: { $0.alwaysBounceHorizontal = $1 }).toAnyStyle
+    }
+    
+    public static func canCancelContentTouches(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .canCancelContentTouches, sideEffect: { $0.canCancelContentTouches = $1 }).toAnyStyle
+    }
+    
+    public static func delaysContentTouches(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .delaysContentTouches, sideEffect: { $0.delaysContentTouches = $1 }).toAnyStyle
+    }
+    
+    public static func indicatorStyle(_ value: UIScrollViewIndicatorStyle) -> AnyStyle<T> {
+        return ViewStyle(value, key: .indicatorStyle, sideEffect: { $0.indicatorStyle = $1 }).toAnyStyle
+    }
+    
+    public static func scrollIndicatorInsets(_ value: UIEdgeInsets) -> AnyStyle<T> {
+        return ViewStyle(value, key: .scrollIndicatorInsets, sideEffect: { $0.scrollIndicatorInsets = $1 }).toAnyStyle
+    }
+    
+    public static func showsHorizontalScrollIndicator(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .showsHorizontalScrollIndicator, sideEffect: { $0.showsHorizontalScrollIndicator = $1 }).toAnyStyle
+    }
+    
+    public static func showsVerticalScrollIndicator(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .showsVerticalScrollIndicator, sideEffect: { $0.showsVerticalScrollIndicator = $1 }).toAnyStyle
+    }
+    
+    public static func maximumZoomScale(_ value: CGFloat) -> AnyStyle<T> {
+        return ViewStyle(value, key: .maximumZoomScale, sideEffect: { $0.maximumZoomScale = $1 }).toAnyStyle
+    }
+    
+    public static func minimumZoomScale(_ value: CGFloat) -> AnyStyle<T> {
+        return ViewStyle(value, key: .minimumZoomScale, sideEffect: { $0.minimumZoomScale = $1 }).toAnyStyle
+    }
+    
+    public static func bouncesZoom(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .bouncesZoom, sideEffect: { $0.bouncesZoom = $1 }).toAnyStyle
     }
 }
--- ViewComponentsTests/TestUIScrollViewStyle.swift
+++ ViewComponentsTests/TestUIScrollViewStyle.swift
@@ -123,6 +123,86 @@ class TestUIScrollViewStyle: XCTestCase, ViewTestType {
         XCTAssertEqual(view.bouncesZoom, true)
     }
     
+    func testAnyStyleSideEffects() {
+        let view = UIScrollView()
+        
+        view.contentOffset = CGPoint(x: 20, y: 20)
+        AnyStyle<UIScrollView>.contentOffset(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.contentOffset, .zero)
+        
+        view.contentSize = CGSize(width: 20, height: 20)
+        AnyStyle<UIScrollView>.contentSize(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.contentSize, .zero)
+        
+        view.contentInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        AnyStyle<UIScrollView>.contentInset(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.contentInset, .zero)
+        
+        view.isScrollEnabled = false
+        AnyStyle<UIScrollView>.isScrollEnabled(true).sideEffect(on: view)
+        XCTAssertEqual(view.isScrollEnabled, true)
+        
+        view.isDirectionalLockEnabled = false
+        AnyStyle<UIScrollView>.isDirectionalLockEnabled(true).sideEffect(on: view)
+        XCTAssertEqual(view.isDirectionalLockEnabled, true)
+        
+        view.isPagingEnabled = false
+        AnyStyle<UIScrollView>.isPagingEnabled(true).sideEffect(on: view)
+        XCTAssertEqual(view.isPagingEnabled, true)
+        
+        view.bounces = false
+        AnyStyle<UIScrollView>.bounces(true).sideEffect(on: view)
+        XCTAssertEqual(view.bounces, true)
+        
+        view.alwaysBounceVertical = false
+        AnyStyle<UIScrollView>.alwaysBounceVertical(true).sideEffect(on: view)
+        XCTAssertEqual(view.alwaysBounceVertical, true)
+        
+        view.alwaysBounceHorizontal = false
+        AnyStyle<UIScrollView>.alwaysBounceHorizontal(true).sideEffect(on: view)
+        XCTAssertEqual(view.alwaysBounceHorizontal, true)
+        
+        view.canCancelContentTouches = false
+        AnyStyle<UIScrollView>.canCancelContentTouches(true).sideEffect(on: view)
+        XCTAssertEqual(view.canCancelContentTouches, true)
+        
+        view.delaysContentTouches = false
+        AnyStyle<UIScrollView>.delaysContentTouches(true).sideEffect(on: view)
+        XCTAssertEqual(view.delaysContentTouches, true)
+        
+        view.indicatorStyle = .black
+        AnyStyle<UIScrollView>.indicatorStyle(.white).sideEffect(on: view)
+        XCTAssertEqual(view.indicatorStyle, .white)
+        
+        view.scrollIndicatorInsets = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        AnyStyle<UIScrollView>.scrollIndicatorInsets(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.scrollIndicatorInsets, .zero)
+        
+        view.showsHorizontalScrollIndicator = false
+        AnyStyle<UIScrollView>.showsHorizontalScrollIndicator(true).sideEffect(on: view)
+        XCTAssertEqual(view.showsHorizontalScrollIndicator, true)
+        
+        view.showsVerticalScrollIndicator = false
+        AnyStyle<UIScrollView>.showsVerticalScrollIndicator(true).sideEffect(on: view)
+        XCTAssertEqual(view.showsVerticalScrollIndicator, true)
+        
+        view.maximumZoomScale = 0.2
+        AnyStyle<UIScrollView>.maximumZoomScale(0.4).sideEffect(on: view)
+        XCTAssertEqual(view.maximumZoomScale, 0.4)
+        
+        view.minimumZoomScale = 0.2
+        AnyStyle<UIScrollView>.minimumZoomScale(0.4).sideEffect(on: view)
+        XCTAssertEqual(view.minimumZoomScale, 0.4)
+        
+        view.bouncesZoom = false
+        AnyStyle<UIScrollView>.bouncesZoom(true).sideEffect(on: view)
+        XCTAssertEqual(view.bouncesZoom, true)
+        
+        view.bouncesZoom = false
+        Component<UIScrollView>().scrollView(.bouncesZoom(true)).configure(item: view)
+        XCTAssertEqual(view.bouncesZoom, true)
+    }
+    
     func testHashValue() {
         testAccumulatingHashes()
     }
