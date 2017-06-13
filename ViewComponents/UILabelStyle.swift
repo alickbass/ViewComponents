b27e56b9c4e04734bf5176d5e59f9994d6d49e90
Oleksii Dykan <alick_bass@mail.ru>
2017-06-13 15:51:51 +0200

    Refactor UILabelStyle to Style

--- ViewComponents/UILabelStyle.swift
+++ ViewComponents/UILabelStyle.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 
+public enum UILabelStyleKey: Int, Hashable {
+    case text = 44, attributedText, font
+    case textColor, textAlignment, lineBreakMode
+    case isEnabled, adjustsFontSizeToFitWidth, allowsDefaultTighteningForTruncation
+    case baselineAdjustment, minimumScaleFactor, numberOfLines
+    case highlightedTextColor, isHighlighted, shadowColor
+    case shadowOffset
+}
+
 public enum UILabelStyle<T: UILabel>: KeyedStyle {
-    public enum Key: Int, Hashable {
-        case text = 44, attributedText, font
-        case textColor, textAlignment, lineBreakMode
-        case isEnabled, adjustsFontSizeToFitWidth, allowsDefaultTighteningForTruncation
-        case baselineAdjustment, minimumScaleFactor, numberOfLines
-        case highlightedTextColor, isHighlighted, shadowColor
-        case shadowOffset
-    }
+    public typealias Key = UILabelStyleKey
     
     case text(String?)
     case attributedText(NSAttributedString?)
@@ -148,5 +150,73 @@ public enum UILabelStyle<T: UILabel>: KeyedStyle {
 public extension Component where T: UILabel {
     public func label(_ styles: UILabelStyle<T>...) -> Component<T> {
         return adding(styles: styles.lazy.map(AnyStyle.init))
+    }
+}
+
+public extension AnyStyle where T: UILabel {
+    private typealias ViewStyle<Item> = Style<T, Item, UILabelStyleKey>
+    
+    public static func text(_ value: String?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .text, sideEffect: { $0.text = $1 }).toAnyStyle
+    }
+    
+    public static func attributedText(_ value: NSAttributedString?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .attributedText, sideEffect: { $0.attributedText = $1 }).toAnyStyle
+    }
+    
+    public static func font(_ value: UIFont) -> AnyStyle<T> {
+        return ViewStyle(value, key: .font, sideEffect: { $0.font = $1 }).toAnyStyle
+    }
+    
+    public static func textColor(_ value: UIColor) -> AnyStyle<T> {
+        return ViewStyle(value, key: .textColor, sideEffect: { $0.textColor = $1 }).toAnyStyle
+    }
+    
+    public static func textAlignment(_ value: NSTextAlignment) -> AnyStyle<T> {
+        return ViewStyle(value, key: .textAlignment, sideEffect: { $0.textAlignment = $1 }).toAnyStyle
+    }
+    
+    public static func lineBreakMode(_ value: NSLineBreakMode) -> AnyStyle<T> {
+        return ViewStyle(value, key: .lineBreakMode, sideEffect: { $0.lineBreakMode = $1 }).toAnyStyle
+    }
+    
+    public static func isEnabled(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .isEnabled, sideEffect: { $0.isEnabled = $1 }).toAnyStyle
+    }
+    
+    public static func adjustsFontSizeToFitWidth(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .adjustsFontSizeToFitWidth, sideEffect: { $0.adjustsFontSizeToFitWidth = $1 }).toAnyStyle
+    }
+    
+    public static func allowsDefaultTighteningForTruncation(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .allowsDefaultTighteningForTruncation, sideEffect: { $0.allowsDefaultTighteningForTruncation = $1 }).toAnyStyle
+    }
+    
+    public static func baselineAdjustment(_ value: UIBaselineAdjustment) -> AnyStyle<T> {
+        return ViewStyle(value, key: .baselineAdjustment, sideEffect: { $0.baselineAdjustment = $1 }).toAnyStyle
+    }
+    
+    public static func minimumScaleFactor(_ value: CGFloat) -> AnyStyle<T> {
+        return ViewStyle(value, key: .minimumScaleFactor, sideEffect: { $0.minimumScaleFactor = $1 }).toAnyStyle
+    }
+    
+    public static func numberOfLines(_ value: Int) -> AnyStyle<T> {
+        return ViewStyle(value, key: .numberOfLines, sideEffect: { $0.numberOfLines = $1 }).toAnyStyle
+    }
+    
+    public static func highlightedTextColor(_ value: UIColor?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .highlightedTextColor, sideEffect: { $0.highlightedTextColor = $1 }).toAnyStyle
+    }
+    
+    public static func isHighlighted(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .isHighlighted, sideEffect: { $0.isHighlighted = $1 }).toAnyStyle
+    }
+    
+    public static func shadowColor(_ value: UIColor?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .shadowColor, sideEffect: { $0.shadowColor = $1 }).toAnyStyle
+    }
+    
+    public static func shadowOffset(_ value: CGSize) -> AnyStyle<T> {
+        return ViewStyle(value, key: .shadowOffset, sideEffect: { $0.shadowOffset = $1 }).toAnyStyle
     }
 }
--- ViewComponentsTests/TestUILabelStyle.swift
+++ ViewComponentsTests/TestUILabelStyle.swift
@@ -131,6 +131,78 @@ class TestUILabelStyle: XCTestCase, ViewTestType {
         XCTAssertEqual(view.isHighlighted, true)
     }
     
+    func testAnyStyleSideEffects() {
+        let view = UILabel()
+        
+        view.text = "someOtherString"
+        AnyStyle<UILabel>.text("test").sideEffect(on: view)
+        XCTAssertEqual(view.text, "test")
+        
+        view.attributedText = .init(string: "someOtherString")
+        AnyStyle<UILabel>.attributedText(.init(string: "test")).sideEffect(on: view)
+        XCTAssertEqual(view.attributedText, NSAttributedString(string: "test"))
+        
+        view.font = .systemFont(ofSize: 13)
+        AnyStyle<UILabel>.font(.systemFont(ofSize: 12)).sideEffect(on: view)
+        XCTAssertEqual(view.font, .systemFont(ofSize: 12))
+        
+        view.textColor = .green
+        AnyStyle<UILabel>.textColor(.red).sideEffect(on: view)
+        XCTAssertEqual(view.textColor, .red)
+        
+        view.textAlignment = .center
+        AnyStyle<UILabel>.textAlignment(.justified).sideEffect(on: view)
+        XCTAssertEqual(view.textAlignment, .justified)
+        
+        view.lineBreakMode = .byClipping
+        AnyStyle<UILabel>.lineBreakMode(.byTruncatingHead).sideEffect(on: view)
+        XCTAssertEqual(view.lineBreakMode, .byTruncatingHead)
+        
+        view.isEnabled = true
+        AnyStyle<UILabel>.isEnabled(false).sideEffect(on: view)
+        XCTAssertEqual(view.isEnabled, false)
+        
+        view.adjustsFontSizeToFitWidth = true
+        AnyStyle<UILabel>.adjustsFontSizeToFitWidth(false).sideEffect(on: view)
+        XCTAssertEqual(view.adjustsFontSizeToFitWidth, false)
+        
+        view.allowsDefaultTighteningForTruncation = true
+        AnyStyle<UILabel>.allowsDefaultTighteningForTruncation(false).sideEffect(on: view)
+        XCTAssertEqual(view.allowsDefaultTighteningForTruncation, false)
+        
+        view.baselineAdjustment = .alignBaselines
+        AnyStyle<UILabel>.baselineAdjustment(.alignCenters).sideEffect(on: view)
+        XCTAssertEqual(view.baselineAdjustment, .alignCenters)
+        
+        view.minimumScaleFactor = 0.5
+        AnyStyle<UILabel>.minimumScaleFactor(0.7).sideEffect(on: view)
+        XCTAssertEqualWithAccuracy(view.minimumScaleFactor, 0.7, accuracy: 0.001)
+        
+        view.numberOfLines = 12
+        AnyStyle<UILabel>.numberOfLines(0).sideEffect(on: view)
+        XCTAssertEqual(view.numberOfLines, 0)
+        
+        view.highlightedTextColor = .green
+        AnyStyle<UILabel>.highlightedTextColor(.red).sideEffect(on: view)
+        XCTAssertEqual(view.highlightedTextColor, .red)
+        
+        view.isHighlighted = true
+        AnyStyle<UILabel>.isHighlighted(false).sideEffect(on: view)
+        XCTAssertEqual(view.isHighlighted, false)
+        
+        view.shadowColor = .green
+        AnyStyle<UILabel>.shadowColor(.red).sideEffect(on: view)
+        XCTAssertEqual(view.shadowColor, .red)
+        
+        view.shadowOffset = CGSize(width: 20, height: 20)
+        AnyStyle<UILabel>.shadowOffset(.zero).sideEffect(on: view)
+        XCTAssertEqual(view.shadowOffset, .zero)
+        
+        view.isHighlighted = false
+        Component<UILabel>().label(.isHighlighted(true)).configure(item: view)
+        XCTAssertEqual(view.isHighlighted, true)
+    }
+    
     func testHashValue() {
         testAccumulatingHashes()
     }
