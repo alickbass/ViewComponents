33087f3e9bbb26545e2f1826d076692da8706e83
Oleksii Dykan <alick_bass@mail.ru>
2017-06-14 09:50:22 +0200

    Remove old activity indicator style

--- ViewComponents/ActivityIndicatorViewStyle.swift
+++ ViewComponents/ActivityIndicatorViewStyle.swift
@@ -8,68 +8,8 @@
 
 import UIKit
 
-public enum ActivityIndicatorViewStyleKey: Int, Hashable {
+private enum ActivityIndicatorViewStyleKey: Int, Hashable {
     case isAnimating = 201, hidesWhenStopped, activityIndicatorViewStyle, color
-}
-
-public enum ActivityIndicatorViewStyle<T: UIActivityIndicatorView>: KeyedStyle {
-    public typealias Key = ActivityIndicatorViewStyleKey
-    
-    case isAnimating(Bool)
-    case hidesWhenStopped(Bool)
-    case activityIndicatorViewStyle(UIActivityIndicatorViewStyle)
-    case color(UIColor?)
-    
-    public func sideEffect(on view: T) {
-        switch self {
-        case let .isAnimating(value):
-            switch value {
-            case true:
-                view.startAnimating()
-            case false:
-                view.stopAnimating()
-            }
-        case let .hidesWhenStopped(value):
-            view.hidesWhenStopped = value
-        case let .activityIndicatorViewStyle(value):
-            view.activityIndicatorViewStyle = value
-        case let .color(value):
-            view.color = value
-        }
-    }
-    
-    public static func == (lhs: ActivityIndicatorViewStyle<T>, rhs: ActivityIndicatorViewStyle<T>) -> Bool {
-        switch (lhs, rhs) {
-        case let (.isAnimating(left), .isAnimating(right)),
-             let (.hidesWhenStopped(left), .hidesWhenStopped(right)):
-            return left == right
-        case let (.activityIndicatorViewStyle(left), .activityIndicatorViewStyle(right)):
-            return left == right
-        case let (.color(left), .color(right)):
-            return left == right
-        default:
-            return false
-        }
-    }
-    
-    @inline(__always) public func value() -> (key: Key, valueHash: Int) {
-        switch self {
-        case let .isAnimating(value):
-            return (.isAnimating, value.hashValue)
-        case let .hidesWhenStopped(value):
-            return (.hidesWhenStopped, value.hashValue)
-        case let .activityIndicatorViewStyle(value):
-            return (.activityIndicatorViewStyle, value.hashValue)
-        case let .color(value):
-            return (.color, value?.hashValue ?? 0)
-        }
-    }
-}
-
-public extension Component where T: UIActivityIndicatorView {
-    public func activityIndicator(_ styles: ActivityIndicatorViewStyle<T>...) -> Component<T> {
-        return adding(styles: styles.lazy.map(AnyStyle.init))
-    }
 }
 
 public extension AnyStyle where T: UIActivityIndicatorView {
--- ViewComponentsTests/TestActivityIndicatorViewStyle.swift
+++ ViewComponentsTests/TestActivityIndicatorViewStyle.swift
@@ -18,40 +18,6 @@ class TestActivityIndicatorViewStyle: XCTestCase, ViewTestType {
     
     static var previousHashes: [Int : Any] { return TestUISliderStyle.accumulatedHashes }
     
-    func testStyleEquatable() {
-        XCTAssertEqual(ActivityIndicatorViewStyle.isAnimating(true), .isAnimating(true))
-        XCTAssertEqual(ActivityIndicatorViewStyle.hidesWhenStopped(true), .hidesWhenStopped(true))
-        XCTAssertEqual(ActivityIndicatorViewStyle.activityIndicatorViewStyle(.gray), .activityIndicatorViewStyle(.gray))
-        XCTAssertEqual(ActivityIndicatorViewStyle.color(.gray), .color(.gray))
-        XCTAssertNotEqual(ActivityIndicatorViewStyle.color(.gray), .activityIndicatorViewStyle(.gray))
-    }
-    
-    func testStyleSideEffects() {
-        let view = UIActivityIndicatorView()
-        
-        view.stopAnimating()
-        ActivityIndicatorViewStyle.isAnimating(true).sideEffect(on: view)
-        XCTAssertEqual(view.isAnimating, true)
-        ActivityIndicatorViewStyle.isAnimating(false).sideEffect(on: view)
-        XCTAssertEqual(view.isAnimating, false)
-        
-        view.hidesWhenStopped = false
-        ActivityIndicatorViewStyle.hidesWhenStopped(true).sideEffect(on: view)
-        XCTAssertEqual(view.hidesWhenStopped, true)
-        
-        view.activityIndicatorViewStyle = .whiteLarge
-        ActivityIndicatorViewStyle.activityIndicatorViewStyle(.gray).sideEffect(on: view)
-        XCTAssertEqual(view.activityIndicatorViewStyle, .gray)
-        
-        view.color = nil
-        ActivityIndicatorViewStyle.color(.gray).sideEffect(on: view)
-        XCTAssertEqual(view.color, .gray)
-        
-        view.color = nil
-        Component<UIActivityIndicatorView>().activityIndicator(.color(.gray)).configure(item: view)
-        XCTAssertEqual(view.color, .gray)
-    }
-    
     func testAnyStyleSideEffects() {
         let view = UIActivityIndicatorView()
         
