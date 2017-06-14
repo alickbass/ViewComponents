157ebbe05f74c3b5d14d878910faace338da1772
Oleksii Dykan <alick_bass@mail.ru>
2017-06-14 09:43:28 +0200

    Remove old colletionviewstyle

--- ViewComponents/UICollectionViewStyle.swift
+++ ViewComponents/UICollectionViewStyle.swift
@@ -13,69 +13,6 @@ public enum UICollectionViewStyleKey: Int, Hashable {
     case remembersLastFocusedIndexPath, collectionViewLayout
 }
 
-public enum UICollectionViewStyle<T: UICollectionView>: KeyedStyle {
-    public typealias Key = UICollectionViewStyleKey
-    
-    @available(iOS 10.0, *)
-    case isPrefetchingEnabled(Bool)
-    case allowsSelection(Bool)
-    case allowsMultipleSelection(Bool)
-    case remembersLastFocusedIndexPath(Bool)
-    case collectionViewLayout(UICollectionViewLayout)
-    
-    public func sideEffect(on view: T) {
-        switch self {
-        case let .isPrefetchingEnabled(value):
-            if #available(iOSApplicationExtension 10.0, *) {
-                view.isPrefetchingEnabled = value
-            }
-        case let .allowsSelection(value):
-            view.allowsSelection = value
-        case let .allowsMultipleSelection(value):
-            view.allowsMultipleSelection = value
-        case let .remembersLastFocusedIndexPath(value):
-            view.remembersLastFocusedIndexPath = value
-        case let .collectionViewLayout(value):
-            view.collectionViewLayout = value
-        }
-    }
-    
-    public static func == (lhs: UICollectionViewStyle<T>, rhs: UICollectionViewStyle<T>) -> Bool {
-        switch (lhs, rhs) {
-        case let (.isPrefetchingEnabled(left), .isPrefetchingEnabled(right)),
-             let (.allowsSelection(left), .allowsSelection(right)),
-             let (.allowsMultipleSelection(left), .allowsMultipleSelection(right)),
-             let (.remembersLastFocusedIndexPath(left), .remembersLastFocusedIndexPath(right)):
-            return left == right
-        case let (.collectionViewLayout(left), .collectionViewLayout(right)):
-            return left == right
-        default:
-            return false
-        }
-    }
-    
-    @inline(__always) public func value() -> (key: Key, valueHash: Int) {
-        switch self {
-        case let .isPrefetchingEnabled(value):
-            return (.isPrefetchingEnabled, value.hashValue)
-        case let .allowsSelection(value):
-            return (.allowsSelection, value.hashValue)
-        case let .allowsMultipleSelection(value):
-            return (.allowsMultipleSelection, value.hashValue)
-        case let .remembersLastFocusedIndexPath(value):
-            return (.remembersLastFocusedIndexPath, value.hashValue)
-        case let .collectionViewLayout(value):
-            return (.collectionViewLayout, value.hashValue)
-        }
-    }
-}
-
-public extension Component where T: UICollectionView {
-    public func collectionView(_ styles: UICollectionViewStyle<T>...) -> Component<T> {
-        return adding(styles: styles.lazy.map(AnyStyle.init))
-    }
-}
-
 public extension AnyStyle where T: UICollectionView {
     private typealias ViewStyle<Item> = Style<T, Item, UICollectionViewStyleKey>
     
--- ViewComponentsTests/TestUICollectionViewStyle.swift
+++ ViewComponentsTests/TestUICollectionViewStyle.swift
@@ -20,42 +20,6 @@ class TestUICollectionViewStyle: XCTestCase, ViewTestType {
     
     let layout = UICollectionViewLayout()
     
-    func testStyleEquatable() {
-        XCTAssertEqual(UICollectionViewStyle.isPrefetchingEnabled(true), .isPrefetchingEnabled(true))
-        XCTAssertEqual(UICollectionViewStyle.allowsSelection(true), .allowsSelection(true))
-        XCTAssertEqual(UICollectionViewStyle.allowsMultipleSelection(true), .allowsMultipleSelection(true))
-        XCTAssertEqual(UICollectionViewStyle.remembersLastFocusedIndexPath(true), .remembersLastFocusedIndexPath(true))
-        XCTAssertEqual(UICollectionViewStyle.collectionViewLayout(layout), .collectionViewLayout(layout))
-        XCTAssertNotEqual(UICollectionViewStyle.collectionViewLayout(layout), .remembersLastFocusedIndexPath(true))
-    }
-    
-    func testStyleSideEffects() {
-        let view = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
-        
-        view.isPrefetchingEnabled = false
-        UICollectionViewStyle.isPrefetchingEnabled(true).sideEffect(on: view)
-        XCTAssertEqual(view.isPrefetchingEnabled, true)
-        
-        view.allowsSelection = false
-        UICollectionViewStyle.allowsSelection(true).sideEffect(on: view)
-        XCTAssertEqual(view.allowsSelection, true)
-        
-        view.allowsMultipleSelection = false
-        UICollectionViewStyle.allowsMultipleSelection(true).sideEffect(on: view)
-        XCTAssertEqual(view.allowsMultipleSelection, true)
-        
-        view.remembersLastFocusedIndexPath = false
-        UICollectionViewStyle.remembersLastFocusedIndexPath(true).sideEffect(on: view)
-        XCTAssertEqual(view.remembersLastFocusedIndexPath, true)
-        
-        UICollectionViewStyle.collectionViewLayout(layout).sideEffect(on: view)
-        XCTAssertEqual(view.collectionViewLayout, layout)
-        
-        view.remembersLastFocusedIndexPath = false
-        Component<UICollectionView>().collectionView(.remembersLastFocusedIndexPath(true)).configure(item: view)
-        XCTAssertEqual(view.remembersLastFocusedIndexPath, true)
-    }
-    
     func testAnyStyleSideEffects() {
         let view = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
         
@@ -77,10 +41,6 @@ class TestUICollectionViewStyle: XCTestCase, ViewTestType {
         
         AnyStyle<UICollectionView>.collectionViewLayout(layout).sideEffect(on: view)
         XCTAssertEqual(view.collectionViewLayout, layout)
-        
-        view.remembersLastFocusedIndexPath = false
-        Component<UICollectionView>().collectionView(.remembersLastFocusedIndexPath(true)).configure(item: view)
-        XCTAssertEqual(view.remembersLastFocusedIndexPath, true)
     }
     
     func testHashValue() {
