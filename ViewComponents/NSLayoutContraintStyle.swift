3ce3ac5b92e3feddfbfdb0cefbc12c73dfbae0f4
Oleksii Dykan <alick_bass@mail.ru>
2017-06-15 16:55:54 +0200

    Add NSLayoutConstraint style

---
+++ ViewComponents/NSLayoutContraintStyle.swift
@@ -0,0 +1,37 @@
+//
+//  NSLayoutContraintStyle.swift
+//  ViewComponents
+//
+//  Created by Oleksii on 15/06/2017.
+//  Copyright © 2017 WeAreReasonablePeople. All rights reserved.
+//
+
+import UIKit
+
+private enum NSLayoutConstraintKey: Int {
+    case isActive = 210, constant, priority, identifier, shouldBeArchived
+}
+
+public extension AnyStyle where T: NSLayoutConstraint {
+    private typealias ViewStyle<Item> = Style<T, Item, NSLayoutConstraintKey>
+    
+    public static func isActive(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .isActive, sideEffect: { $0.isActive = $1 }).toAnyStyle
+    }
+    
+    public static func constant(_ value: CGFloat) -> AnyStyle<T> {
+        return ViewStyle(value, key: .constant, sideEffect: { $0.constant = $1 }).toAnyStyle
+    }
+    
+    public static func priority(_ value: UILayoutPriority) -> AnyStyle<T> {
+        return ViewStyle(value, key: .priority, sideEffect: { $0.priority = $1 }).toAnyStyle
+    }
+    
+    public static func identifier(_ value: String?) -> AnyStyle<T> {
+        return ViewStyle(value, key: .identifier, sideEffect: { $0.identifier = $1 }).toAnyStyle
+    }
+    
+    public static func shouldBeArchived(_ value: Bool) -> AnyStyle<T> {
+        return ViewStyle(value, key: .shouldBeArchived, sideEffect: { $0.shouldBeArchived = $1 }).toAnyStyle
+    }
+}
---
+++ ViewComponentsTests/TestNSLayoutConstraintStyle.swift
@@ -0,0 +1,49 @@
+//
+//  TestNSLayoutConstraintStyle.swift
+//  ViewComponents
+//
+//  Created by Oleksii on 15/06/2017.
+//  Copyright © 2017 WeAreReasonablePeople. All rights reserved.
+//
+
+import XCTest
+import ViewComponents
+
+class TestNSLayoutConstraintStyle: XCTestCase, ViewTestType {
+    
+    static let allStyles: [AnyStyle<NSLayoutConstraint>] = [
+        .isActive(true), AnyStyle<NSLayoutConstraint>.constant(20),
+        .priority(200), .identifier("some"), .shouldBeArchived(true)
+    ]
+    
+    static var previousHashes: [Int : Any] { return TestUISwitchStyle.accumulatedHashes }
+    
+    func testAnyStyleSideEffects() {
+        let view = UIView()
+        let constraint = view.widthAnchor.constraint(equalToConstant: 10)
+        
+        constraint.isActive = false
+        AnyStyle<NSLayoutConstraint>.isActive(true).sideEffect(on: constraint)
+        XCTAssertEqual(constraint.isActive, true)
+        
+        constraint.constant = 30
+        AnyStyle<NSLayoutConstraint>.constant(20).sideEffect(on: constraint)
+        XCTAssertEqual(constraint.constant, 20)
+        
+        constraint.priority = 750
+        AnyStyle<NSLayoutConstraint>.priority(200).sideEffect(on: constraint)
+        XCTAssertEqual(constraint.priority, 200)
+        
+        constraint.identifier = nil
+        AnyStyle<NSLayoutConstraint>.identifier("test").sideEffect(on: constraint)
+        XCTAssertEqual(constraint.identifier, "test")
+        
+        constraint.isActive = false
+        AnyStyle<NSLayoutConstraint>.shouldBeArchived(true).sideEffect(on: constraint)
+        XCTAssertEqual(constraint.shouldBeArchived, true)
+    }
+    
+    func testHashValue() {
+        testAccumulatingHashes()
+    }
+}
