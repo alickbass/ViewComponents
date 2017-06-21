4ac8e2c02897093c34b0b9b9e9d5d8b63f2974b4
Oleksii Dykan <alick_bass@mail.ru>
2017-06-21 11:04:40 +0200

    Add data source and delegate for uicollection view

--- ViewComponentsTests/TestUICollectionViewStyle.swift
+++ ViewComponentsTests/TestUICollectionViewStyle.swift
@@ -9,16 +9,34 @@
 import XCTest
 import ViewComponents
 
+class CollectionViewController: NSObject, UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return .init()
+    }
+}
+
 class TestUICollectionViewStyle: XCTestCase, ViewTestType {
     
     static let allStyles: [AnyStyle<UICollectionView>] = [
         .isPrefetchingEnabled(true), .allowsSelection(true), .allowsMultipleSelection(true),
-        .remembersLastFocusedIndexPath(true), .collectionViewLayout(UICollectionViewLayout())
+        .remembersLastFocusedIndexPath(true), .collectionViewLayout(UICollectionViewLayout()),
+        .dataSource(nil), .dataSource(CollectionViewController()), .delegate(nil),
+        .delegate(CollectionViewController())
     ]
     
     static var previousHashes: [Int : Any] { return TestTableViewCellStyle.accumulatedHashes }
     
     let layout = UICollectionViewLayout()
+    let controller = CollectionViewController()
+    
+    func testAnyStyleEquatable() {
+        XCTAssertEqual(AnyStyle<UICollectionView>.dataSource(controller), .dataSource(controller))
+        XCTAssertEqual(AnyStyle<UICollectionView>.delegate(controller), .delegate(controller))
+    }
     
     func testAnyStyleSideEffects() {
         let view = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
@@ -41,6 +59,14 @@ class TestUICollectionViewStyle: XCTestCase, ViewTestType {
         
         AnyStyle<UICollectionView>.collectionViewLayout(layout).sideEffect(on: view)
         XCTAssertEqual(view.collectionViewLayout, layout)
+        
+        view.dataSource = nil
+        AnyStyle<UICollectionView>.dataSource(controller).sideEffect(on: view)
+        XCTAssertTrue(view.dataSource === controller)
+        
+        view.delegate = nil
+        AnyStyle<UICollectionView>.delegate(controller).sideEffect(on: view)
+        XCTAssertTrue(view.delegate === controller)
     }
     
     func testHashValue() {
